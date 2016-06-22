a73e9f70a961d6eb1e75065c582ebdb1c34425ae
Zhicheng <Zhicheng_qiu@cable.comcast.com>
2016-06-22 12:23:11 -0700

    RDKB-6279:
    Reason for change: Add switch to turn on/off data monitoring
    Test Procedure: when rxtx_enabled is not "1", the ebtable should be empty
    Risks: None
    Signed-off-by: Zhicheng <Zhicheng_qiu@cable.comcast.com>
    
    Change-Id: I23c3e84572268af7ca4c958e675b48e5463dd3b6
    Signed-off-by: Zhicheng <Zhicheng_qiu@cable.comcast.com>

--- scripts/rxtx_cur.sh
+++ scripts/rxtx_cur.sh
@@ -2,8 +2,8 @@
 #zhicheng_qiu@cable.comcast.com
 
 #!/bin/sh
-ebtables -L --Lc | grep CONTINUE  | sort -k 2,2 | sed "N;s/\n/ /" | cut -d' ' -f2,8,12,20,24 | tr " " "|" > /tmp/rxtx_cur.txt
-cat /tmp/rxtx_cur.txt | cut -d'|' -f1 > /tmp/eblist
+ebtables -L --Lc | grep CONTINUE  | sort -k 2,2 | sed "N;s/\n/ /" | cut -d' ' -f2,8,12,20,24 | awk 'BEGIN{FS="[: ]";}{ printf "%2s:%2s:%2s:%2s:%2s:%2s|%s|%s|%s|%s\n", $1,$2,$3,$4,$5,$6,$7,$8,$8,$10; }' | tr ' ' '0' | sort -u > /tmp/rxtx_cur.txt
+cat /tmp/rxtx_cur.txt | cut -d'|' -f1 | sort -u > /tmp/eblist
 cat /nvram/dnsmasq.leases | grep -v "\* \*" | grep -v "172.16.12." | cut -d' ' -f2 > /tmp/cli4
 ip nei show | grep brlan0 | grep -v FAILED | cut -d' ' -f 5  > /tmp/cli46
 cat /tmp/cli4 /tmp/cli46 | sort -u > /tmp/clilist
--- scripts/rxtx_dmp.sh
+++ scripts/rxtx_dmp.sh
@@ -1,6 +1,10 @@
 #This is used to save the total rxtx counter before reboot
 #zhicheng_qiu@cable.comcast.com
 #!/bin/sh
+rxtx_enabled=`syscfg get rxtx_enabled`
+if [ "$rxtx_enabled" != "1" ]; then
+	exit 0;
+fi
 /fss/gw/usr/ccsp/tad/rxtx_sum.sh 
 cp /tmp/rxtx_sum.txt /nvram/rxtx_bak.txt
 
