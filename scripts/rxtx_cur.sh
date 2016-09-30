1d1d9159d2f633189a2e5d749cc241da9b689e07
Zhicheng <Zhicheng_qiu@cable.comcast.com>
2016-09-30 13:41:15 -0700

    RDKB-7929: Need to hookup traffic_count
    Reason for change: Need to hookup traffic_count in traffic count script
    Test Procedure: run rxtx_cur.sh when stream video to client
    Risks: None
    Signed-off-by: Zhicheng <Zhicheng_qiu@cable.comcast.com>
    
    Change-Id: Ic09bee4b3e9983538c176c2798c95002d243c2eb

--- scripts/rxtx_cur.sh
+++ scripts/rxtx_cur.sh
@@ -2,14 +2,17 @@
 #zhicheng_qiu@cable.comcast.com
 
 #!/bin/sh
-ebtables -L --Lc | grep CONTINUE  | sort -k 2,2 | sed "N;s/\n/ /" | cut -d' ' -f2,8,12,20,24 | awk 'BEGIN{FS="[: ]";}{ printf "%2s:%2s:%2s:%2s:%2s:%2s|%s|%s|%s|%s\n", $1,$2,$3,$4,$5,$6,$7,$8,$8,$10; }' | tr ' ' '0' | sort -u > /tmp/rxtx_cur.txt
+#ebtables -L --Lc | grep CONTINUE  | sort -k 2,2 | sed "N;s/\n/ /" | cut -d' ' -f2,8,12,20,24 | awk 'BEGIN{FS="[: ]";}{ printf "%2s:%2s:%2s:%2s:%2s:%2s|%s|%s|%s|%s\n", $1,$2,$3,$4,$5,$6,$7,$8,$8,$10; }' | tr ' ' '0' | sort -u > /tmp/rxtx_cur.txt
+traffic_count -L | tr '[a-z]' '[A-Z]' > /tmp/rxtx_cur.txt
+
 cat /tmp/rxtx_cur.txt | cut -d'|' -f1 | sort -u > /tmp/eblist
-cat /nvram/dnsmasq.leases | grep -v "\* \*" | grep -v "172.16.12." | cut -d' ' -f2 > /tmp/cli4
+cat /tmp/dnsmasq.leases | grep -v "\* \*" | grep -v "172.16.12." | cut -d' ' -f2 > /tmp/cli4
 ip nei show | grep brlan0 | grep -v FAILED | cut -d' ' -f 5  > /tmp/cli46
-cat /tmp/cli4 /tmp/cli46 | sort -u > /tmp/clilist
+cat /tmp/cli4 /tmp/cli46 | sort -u | tr '[a-z]' '[A-Z]'  > /tmp/clilist
 diff /tmp/eblist /tmp/clilist | grep "^+" | grep -v "+++" | cut -d'+' -f2 > /tmp/nclilist
 for mac in $(cat /tmp/nclilist); do
-  ebtables -A INPUT -s $mac
-  ebtables -A OUTPUT -d $mac
+  #ebtables -A INPUT -s $mac
+  #ebtables -A OUTPUT -d $mac
+  traffic_count -A $mac
 done
 
