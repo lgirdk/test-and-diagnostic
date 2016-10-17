f6bdc09163221996b20f6389c8f4bd68cafa7e05
kdasari <Krishna_Dasari@cable.comcast.com>
2016-10-17 12:00:00 -0400

    RDKB-7784 : Develop TR-181 parameter to enable and run speedtest2
    Reason for change: Added rpcclient command to invoke speedtest script on atom side
    Test Procedure: Set TR181 parameter to run speedtest and it should invoke run_speedtest.sh on atom side
    Risks: None
    Signed-off-by: Krishna_Dasari@cable.comcast.com
    
    Change-Id: I157ee7a3909a38b1b801c20f2d0f0e9a8424310f

--- scripts/speedtest.sh
+++ scripts/speedtest.sh
@@ -2,3 +2,10 @@
 
 #!/bin/sh
 echo "Enabling / Starting speedtest..." > /dev/console
+. /etc/device.properties
+if [ "$BOX_TYPE" = XB3 ]
+then
+  rpcclient 192.168.254.254 "/etc/speedtest/run_speedtest.sh"
+else
+  echo "Unsupported device model" > /dev/console
+fi
