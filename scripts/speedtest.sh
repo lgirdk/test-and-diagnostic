49d50f372872883bf904d2694917dc7dbc0e1c07
kdasari <Krishna_Dasari@cable.comcast.com>
2016-11-04 16:29:40 +0000

    ARRISXB6-2632: Modify speedtest shell script
    Reason for change: Added support for xb6
    Test Procedure: Speedtest should run on triggering below TR181 parameter
                    Device.IP.Diagnostics.X_RDKCENTRAL-COM_SpeedTest.Enable_Speedtest
                    Device.IP.Diagnostics.X_RDKCENTRAL-COM_SpeedTest.run
    Risks: None
    Signed-off-by: Krishna Dasari<Krishna_Dasari@cable.comcast.com>
    
    Change-Id: If659d3a703c23810f566d4576707720aa684026d

--- scripts/speedtest.sh
+++ scripts/speedtest.sh
@@ -5,7 +5,12 @@ echo "Enabling / Starting speedtest..." > /dev/console
 . /etc/device.properties
 if [ "$BOX_TYPE" = XB3 ]
 then
-  rpcclient 192.168.254.254 "/etc/speedtest/run_speedtest.sh"
+  echo "Executing run_speedtest.sh for xb3" > /dev/console
+  rpcclient $ATOM_ARPING_IP "/etc/speedtest/run_speedtest.sh"
+elif [ "$BOX_TYPE" = XB6 ]
+then
+  echo "Executing run_speedtest.sh for xb6" > /dev/console
+  sh /etc/speedtest/run_speedtest.sh
 else
   echo "Unsupported device model" > /dev/console
 fi
