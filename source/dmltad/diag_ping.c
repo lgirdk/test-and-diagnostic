0580c28daa69e838a67cee990be5dc793f0870b2
dravin003c <Dhanya_Ravindran@comcast.com>
2016-11-22 05:04:44 +0000

    RDKB-8600 :IPPing.MinimumResponseTime is not accurate
    Reason for change: Modified awk command in diag_ping.c to fetch minumum and maximum values correctly.
    Test Procedure: None
    Risks: None.
    Signed-off-by: Dhanya_Ravindran2@comcast.com
    
    Change-Id: Ib43edb25bc37a298b31858d486e20e2894783ec5

--- source/dmltad/diag_ping.c
+++ source/dmltad/diag_ping.c
@@ -94,7 +94,7 @@ static diag_err_t ping_start(diag_obj_t *diag, const diag_cfg_t *cfg, diag_stat_
     const char      *awkcmd = 
         " | awk -F'[ /]+' '/transmitted/ {SEND=$1; RECV=$4; }; "
         " /^ping:/ { print; exit } " /* capture ping error message */
-        " /^round-trip/ {MAX=$6; AVG=$7; MIN=$8 } "
+        " /^round-trip/ {MIN=$6; AVG=$7; MAX=$8 } "
         " /^rtt/ {MIN=$7; AVG=$8; MAX=$9 } "
         " END {print SEND, RECV, MIN, AVG, MAX}' 2>&1";
 
