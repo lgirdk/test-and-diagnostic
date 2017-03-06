a53b6f6584697284c60c4108bf5e1aa3f6f3f9ba
Karthik Shanbhag <karthik_shanbhag@comcast.com>
2017-03-06 20:54:37 +0530

    RDKB-9367 : Log file-nr and nr_open in selfheal
    Reason for change: Log file-nr and nr_open in selfheal
    Test Procedure: Every 12 hours file-nr and nr_open values should be logged to selfheal.txt
    700105-12:01:11.220000 [RDKB_SELHEAL]Output of file-nr /proc/sys/fs/file-nr 5948 0       24982
    700105-12:01:11.650000 [RDKB_SELHEAL]Output of nr_open /proc/sys/fs/nr_open 1048576
    Risks: None
    
    Change-Id: Ib0844272703a75d568f7280d2c4d0fd67b95e162
    Signed-off-by: Karthik Shanbhag karthik_shanbhag@comcast.com

---
+++ scripts/FileHandle_Monitor.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+UTOPIA_PATH="/etc/utopia/service.d"
+	
+source $UTOPIA_PATH/log_env_var.sh
+source $UTOPIA_PATH/log_timestamp.sh
+
+exec 3>&1 4>&2 >>$SELFHEALFILE 2>&1
+
+file_nr="/proc/sys/fs/file-nr"
+nr_open="/proc/sys/fs/nr_open"
+
+echo_t "[RDKB_SELHEAL]Output of file-nr $file_nr `cat $file_nr`"
+echo_t "[RDKB_SELHEAL]Output of nr_open $nr_open `cat $nr_open`"
