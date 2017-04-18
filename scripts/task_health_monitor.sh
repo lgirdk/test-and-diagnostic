1981b949bb3734012fb66bf7f07c8f27329c8594
Santosh Nayak <Santosh_Nayak03@infosys.com>
2017-04-18 05:59:28 -0700

    RDKB-10930 : SelfHeal recovery mechanism when dnsmasq in a bad state.
    Reason for change: dnsmasq was zombie and we are recovering it by restarting dhcp server.
    Test Procedure:
    1) We can't simulate dnsmasq Zombie issue. Please verify basic selfheal functionality for any regression.
    
    Change-Id: I92fdcba6d133e19cb4388adfafca3e56124f2db6
    Risks: Low
    Signed-off-by: Santosh Nayak Santosh_Nayak03@infosys.com

--- scripts/task_health_monitor.sh
+++ scripts/task_health_monitor.sh
@@ -721,6 +721,18 @@ fi
 		         sysevent set dhcp_server-start
 		     fi
 	     fi
+	
+	checkIfDnsmasqIsZombie=`ps | grep dnsmasq | grep "Z" | awk '{ print $1 }'`
+	if [ "$checkIfDnsmasqIsZombie" != "" ] ; then
+		confirmZombie=`grep "State:" /proc/$checkIfDnsmasqIsZombie/status | grep -i "zombie"`
+		if [ "$confirmZombie" != "" ] ; then
+			echo_t "[RDKB_SELFHEAL] : Zombie instance of dnsmasq is present, restarting dnsmasq"
+			kill -9 `pidof dnsmasq`
+			sysevent set dhcp_server-stop
+			sysevent set dhcp_server-start
+		fi
+	fi
+
    fi
 
 #Checking dibbler server is running or not RDKB_10683
