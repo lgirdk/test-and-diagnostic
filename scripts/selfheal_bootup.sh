4db8ebbe015b3030fc4126ee6e93bc1320105872
Karthik Shanbhag <karthik_shanbhag@comcast.com>
2017-05-10 19:08:24 +0530

    RDKB-11597 : Atom hang is reported as CR crash
    Reason for change: Atom hang is reported as CR crash
    Test Procedure: In the case of atom hang reboot reason should be set as peer down.
    Risks: None
    
    Change-Id: Icecd3cd56797a9b1866d8a34655bbf359cfb8b7f
    Signed-off-by: Karthik Shanbhag karthik_shanbhag@comcast.com

--- scripts/selfheal_bootup.sh
+++ scripts/selfheal_bootup.sh
@@ -219,6 +219,7 @@ isIPv6=""
 		Check_WAN_Ip=1
 	 fi
 ## Check Peer ip is accessible
+
 if [ -f $PING_PATH/ping_peer ]
 then
 loop=1
@@ -253,7 +254,7 @@ loop=1
 	         	then
 	    			echo_t "RDKB_SELFHEAL_BOOTUP : RPC Communication with ATOM is OK"
 			else
-			   	echo_t "RDKB_SELFHEAL_BOOTUP : RPC Communication with ATOM is NOK"    
+				echo_t "RDKB_SELFHEAL_BOOTUP : RPC Communication with ATOM is NOK"
 			fi
 	     else
 			echo "Non-XB3 case / ATOM_ARPING_IP is NULL not checking communication using rpcclient"
@@ -324,11 +325,19 @@ fi
                         if [ ! -f "$needSelfhealReboot" ]
                         then
                                 touch $needSelfhealReboot
-                                reason="CR_crash"
+				if [ "$ping_failed" == "1" ]
+				then
+					echo_t "RDKB_PROCESS_CRASHED : Ping peer failed and CR_process not running"
+                                	reason="Peer_down"
+					backupreason=""
+				else
+					reason="CR_crash"
+					backupreason="CR"
+				fi
                                 rebootCount=1
                                 setRebootreason $reason $rebootCount
 
-                                $RDKLOGGER_PATH/backupLogs.sh "true" "CR"
+                                $RDKLOGGER_PATH/backupLogs.sh "true" $backupreason
                         else
                                 rm -rf $needSelfhealReboot
                         fi
