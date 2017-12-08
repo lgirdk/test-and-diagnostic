f1ed95fc8a9e177016eef7ab03e144fd624a32e0
Sudharsan Raman <sudharsan_raman@comcast.com>
2017-12-08 14:08:18 +0530

    ARRISXB3-6945 [Field][Data-Analysis]DHCP range corrupted, dnsmasq not running
    Reason for change: Added logic to recover blan0 netmask if it is corrupted while booting up.
    
    Test Procedure: set invalid netmask using 'syscfg set lan_netmask <invalid netmask>' and 'syscfg commit'.
    reboot the box. The box should comeup with correct blan0 ip address.
    Risks: None
    Signed-off-by: Gokulakrishnan R <gokulakrishnan.r01@infosys.com>
    
    Change-Id: Ib23b6f18e6b758a0dfbfe0b0e2c42675d7f1361f

--- scripts/systemd/sysd_task_health_monitor.sh
+++ scripts/systemd/sysd_task_health_monitor.sh
@@ -351,6 +351,23 @@ LIGHTTPD_CONF="/var/lighttpd.conf"
 		fi
      fi
 
+#Logging to check the DHCP range corruption
+    lan_ipaddr=`syscfg get lan_ipaddr`
+    lan_netmask=`syscfg get lan_netmask`
+    echo "[`getDateTime`] [RDKB_SELFHEAL] : lan_ipaddr = $lan_ipaddr lan_netmask = $lan_netmask"
+
+    lost_and_found_enable=`syscfg get lost_and_found_enable`
+    echo "[`getDateTime`] [RDKB_SELFHEAL] :  lost_and_found_enable = $lost_and_found_enable"
+    if [ "$lost_and_found_enable" == "true" ]
+    then
+        iot_ifname=`syscfg get iot_ifname`
+        iot_dhcp_start=`syscfg get iot_dhcp_start`
+        iot_dhcp_end=`syscfg get iot_dhcp_end`
+        iot_netmask=`syscfg get iot_netmask`
+        echo "[`getDateTime`] [RDKB_SELFHEAL] : DHCP server configuring for IOT iot_ifname = $iot_ifname "
+        echo "[`getDateTime`] [RDKB_SELFHEAL] : iot_dhcp_start = $iot_dhcp_start iot_dhcp_end=$iot_dhcp_end iot_netmask=$iot_netmask"
+    fi
+
 #Checking whether dnsmasq is running or not
    DNS_PID=`pidof dnsmasq`
    if [ "$DNS_PID" == "" ]
