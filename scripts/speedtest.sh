06baeffaed66354add87a8abba09f5eb2c564367
kdasari <Krishna_Dasari@cable.comcast.com>
2018-02-22 17:30:47 -0500

    TCCBR-2563 : Speedtest log file is missing
    Reason for change: Added support to trigger via TR181
    Test Procedure: Able to run speedtest and log should be generated
    Risks: Low
    Signed-off-by: Krishna Dasari <Krishna_Dasari@cable.comcast.com>
    
    Change-Id: I818bfb9bfaf4757c6b4d9071231810020a0b3c99

--- scripts/speedtest.sh
+++ scripts/speedtest.sh
@@ -7,7 +7,7 @@ if [ "$BOX_TYPE" = XB3 ]
 then
   echo "Executing run_speedtest.sh for xb3" > /dev/console
   rpcclient $ATOM_ARPING_IP "/etc/speedtest/run_speedtest.sh"
-elif [ "$BOX_TYPE" = XB6 ] || [ "$BOX_TYPE" = XF3 ]
+elif [ "$BOX_TYPE" = XB6 ] || [ "$BOX_TYPE" = XF3 ] || [ "$BOX_TYPE" = TCCBR ]
 then
   echo "Executing run_speedtest.sh" > /dev/console
   sh /etc/speedtest/run_speedtest.sh
