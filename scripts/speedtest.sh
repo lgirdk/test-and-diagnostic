74dfd19a146268e9b3ecb1a8e26d2c379e4aa955
David Smith <dave.m.smith@accenture.com>
2018-06-18 17:55:15 +0100

    RDKCOM-725 RDKBCMF-200: Correct shebang placement.
    Reason for change: Shebang placement incorrect.
    Test Procedure: Build only.
    Risks: None
    Signed-off-by: Dave Smith <dave.m.smith@accenture.com>
    
    Change-Id: I2dd2cf55d8c755c7ec46e305b7c986d2e1e2cecb

--- scripts/speedtest.sh
+++ scripts/speedtest.sh
@@ -1,15 +1,16 @@
+#!/bin/sh
 #######################################################################################
 # If not stated otherwise in this file or this component's Licenses.txt file the
 # following copyright and licenses apply:
-
+#
 #  Copyright 2018 RDK Management
-
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-
+#
 # http://www.apache.org/licenses/LICENSE-2.0
-
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,7 +20,6 @@
 
 #This script is used to enable / start the speedtest tool
 
-#!/bin/sh
 echo "Enabling / Starting speedtest..." > /dev/console
 . /etc/device.properties
 if [ "$BOX_TYPE" = XB3 ]
