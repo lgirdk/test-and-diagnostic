600578670ad48ebcd94ec824817170adb32198d5
hchen266 <harvey_chen@comcast.com>
2018-11-13 19:55:04 +0000

    RDKB-20366 Observed "No such file or directory" for cosa_start_rem.sh
    Reason for change: not all platforms need to run this script,
    some platforms like AXB3 need to run the following script,
    but some platforms like TCXB6 use systemd and does not need it,
    only start the script if it exists
    Test Procedure: boot up the new image, and check if there is error in
    console log,
    Risks: None
    Signed-off-by: harvey_chen@comcast.com
    
    (cherry picked from commit 6624e34e547f73029020a69c56168de6c3037641)
    
    Change-Id: I7f980da248190a921c501bc21fd24fc2f24d832c

--- scripts/selfheal_cosa_start_rem.sh
+++ scripts/selfheal_cosa_start_rem.sh
@@ -14,6 +14,11 @@ if [ -f /tmp/cosa_start_rem_triggered ]; then
 	rm -rf /etc/cron/cron.every10minute/selfheal_cosa_start_rem.sh
 else
 	echo "cosa_start_rem script not triggered even after 10 minutes from boot-up so start from selfheal"
-	sh /usr/ccsp/cosa_start_rem.sh &	
+	# some platforms like AXB3 need to run the following script,
+	# but some platforms like TCXB6 use systemd and does not need it,
+	# only start the script if it exists
+	if [ -f /usr/ccsp/cosa_start_rem.sh ]; then
+		sh /usr/ccsp/cosa_start_rem.sh &	
+	fi
 	rm -rf /etc/cron/cron.every10minute/selfheal_cosa_start_rem.sh
 fi
