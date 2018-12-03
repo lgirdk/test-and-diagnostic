df6b8fa8bea9110f1fa58f6b5f2551cf1a91c29c
lshenb001c <Lakshminarayanan_Shenbagaraj@comcast.com>
2018-12-03 14:08:00 +0530

    ARRISXB3-8366 : Observing syntax error in log_mem_cpu_info.sh.
    Reason for change:
    Observing below error in Self Heal Logs due to syntax problem introduced as part of https://gerrit.teamccp.com/#/c/237748/ changes. so needs to be correct this.
    /usr/ccsp/tad/log_mem_cpu_info.sh: line 195: syntax error: unexpected word (expecting ")")
    
    Test Procedure:
    1. Below errors should not come in Self Heal Logs for all devices,
    /usr/ccsp/tad/log_mem_cpu_info.sh: line 195: syntax error: unexpected word (expecting ")")
    
    2. Ensure log_mem_cpu_info.sh script should run by every 1 hour time slice without any issue and it should say proper memory and CPU statistics accordingly.
    
    Risks: None
    Signed-off-by: Lakshminarayanan Shenbagaraj Lakshminarayanan_Shenbagaraj@comcast.com
    (cherry picked from commit 007393975cc639911c368a195e8c6d2c7bb624d2)
    (cherry picked from commit b8dc0df84464ca518d707a5f6eeac75802999351)
    (cherry picked from commit add9ece16ee81d162849645a9ce732bcd95428e5)
    
    Change-Id: I07093d94c74c366573777c7acc4a89d6b1688153

--- scripts/log_mem_cpu_info.sh
+++ scripts/log_mem_cpu_info.sh
@@ -174,7 +174,7 @@ DELAY=30
 		echo_t "[RDKB_SELFHEAL] : NVRAM2 IS READ-ONLY"
 	fi
 
-	if [ "$BOX_TYPE" = "XB3"  || "$MODEL_NUM" = "TG3482G" ]; then
+	if [ "$BOX_TYPE" = "XB3" ]  || [ "$MODEL_NUM" = "TG3482G" ]; then
 		if [ "$UTC_ENABLE" == "true" ]
 		then
 			cur_hr=`LTime H`
@@ -191,9 +191,9 @@ DELAY=30
 				if [ ! -f "/tmp/snmp_agent_restarted" ]; then
 					echo_t  "RDKB_SELFHEAL : Restarting snmp subagent in maintanance window" 
 					touch /tmp/.snmp_agent_restarting
-					if [ "$MODEL_NUM" = "TG3482G" ] ; then
-						system("systemctl restart snmpSubAgent");
-						sleep 2;
+					if [ "$MODEL_NUM" = "TG3482G" ]; then
+						systemctl restart snmpSubAgent
+						sleep 2
 					else
 						SNMP_PID=`ps -ww | grep snmp_subagent | grep -v cm_snmp_ma_2 | grep -v grep | awk '{print $1}'`
 						kill -9 $SNMP_PID
