0ddda3b0311eea5a44577730069429f685ac284c
Sachin Kitnawat <Sachin_Kitnawat@comcast.com>
2019-02-28 11:08:40 +0000

    RDKB-21142 TestAndDiagnostic Unapproved Cisco Copyrights
    Reason for change: Removing cisco unwanted cisco copyright
    Test Procedure: Build using Jenkins
    Risks: None
    
    Signed-off-by: Sachin Kitnawat <Sachin_Kitnawat@comcast.com>
    (cherry picked from commit 7da28d9e1496c3e792639ac3051267428b108178)
    
    Change-Id: I66689d86660bd71470415e652fb43f7c934a5e22

--- source/diagnostic/BbhmDiagIpTraceroute/bbhm_diagit_tdo_operation.c
+++ source/diagnostic/BbhmDiagIpTraceroute/bbhm_diagit_tdo_operation.c
@@ -43,13 +43,6 @@
 
     ---------------------------------------------------------------
 
-    copyright:
-
-        Cisco System  , Inc., 1997 ~ 2001
-        All Rights Reserved.
-
-    ---------------------------------------------------------------
-
     description:
 
         This module implements the advanced operation functions
