5d7b641ecfa1ba5f908f6880c9ab49fbbd4edcc8
dpikor933 <daniel_pikora@comcast.com>
2019-04-18 21:15:59 +0000

    RDKB-20261 RDKB-21729: Add newer platforms to script
    Reason for change: add speedtest-client for Technicolor XB6, Arris XB6,
    CBR, and Cisco XB3 to script
    Test Procedure: run speedtest-client and Node client on each platform
    from the on-demand API
    Risks: low
    Signed-off-by: Daniel Pikora <daniel_pikora@comcast.com>
    
    (cherry picked from commit 01377310812812c3c2c4500fc51b8a214b263cf0)
    (cherry picked from commit c1b3b9c929d42cbb1f089c4bf390836ab6497fef)
    
    Change-Id: I0aa3aaa49a4c6cc039fa59de9f37bfae18f34f5b

--- scripts/speedtest.sh
+++ scripts/speedtest.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #######################################################################################
 # If not stated otherwise in this file or this component's Licenses.txt file the
 # following copyright and licenses apply:
@@ -22,24 +22,86 @@
 
 echo "Enabling / Starting speedtest..." > /dev/console
 . /etc/device.properties
+
+# Two implementations of the speedtest currently co-exist on most of our platforms - a Node.js client, and a C client.
+  # If Device.IP.Diagnostics.X_RDKCENTRAL-COM_SpeedTest.ClientType exists, then:
+  # Device.IP.Diagnostics.X_RDKCENTRAL-COM_SpeedTest.ClientType = 1 implies the C client should run.
+  # Device.IP.Diagnostics.X_RDKCENTRAL-COM_SpeedTest.ClientType = 2 implies the NodeJS client should run.
+ST_CLIENT_TYPE=`dmcli eRT getv Device.IP.Diagnostics.X_RDKCENTRAL-COM_SpeedTest.ClientType | awk '/value/ { print $5 }'`
+
 if [ "$BOX_TYPE" = XB3 ] && [ "$MODEL_NUM" = TG1682G ]
 then
-  # There are two implementations of the speedtest that currently must co-exist on the XB3 - a NodeJS based client, and a newer C/CPP client.
-  # If Device.IP.Diagnostics.X_RDKCENTRAL-COM_SpeedTest.ClientType exists, then:
-  # Device.IP.Diagnostics.X_RDKCENTRAL-COM_SpeedTest.ClientType = 1 implies the C client must be executed.
-  # Device.IP.Diagnostics.X_RDKCENTRAL-COM_SpeedTest.ClientType = 2 implies the NodeJS client must be executed.
-  ST_CLIENT_TYPE=`dmcli eRT getv Device.IP.Diagnostics.X_RDKCENTRAL-COM_SpeedTest.ClientType | grep value | cut -d ":" -f 3 | tr -d ' '`
   if [ "x$ST_CLIENT_TYPE" = 'x1' ]
   then
     # C speedtest client
-    echo "Executing native c speedtest-client for xb3" > /dev/console
-    rpcclient $ATOM_ARPING_IP "/usr/bin/speedtest-client &"
+    echo "Executing native c speedtest-client for xb3a" > /dev/console
+    rpcclient "$ATOM_ARPING_IP" "/usr/bin/speedtest-client &"
   else
     # NodeJS speedtest client
-    echo "Executing run_speedtest.sh for xb3" > /dev/console
-    rpcclient $ATOM_ARPING_IP "/etc/speedtest/run_speedtest.sh &"
+    echo "Executing run_speedtest.sh for xb3a" > /dev/console
+    rpcclient "$ATOM_ARPING_IP" "/etc/speedtest/run_speedtest.sh &"
   fi
-elif [ "$BOX_TYPE" = XB6 ] || [ "$BOX_TYPE" = XF3 ] || [ "$BOX_TYPE" = TCCBR ]
+elif [ "$BOX_TYPE" = XB6 ] && [ "$MODEL_NUM" = TG3482G ]
+then
+  if [ "x$ST_CLIENT_TYPE" = 'x1' ]
+  then
+    # C speedtest client
+    echo "Executing native c speedtest-client for xb6a" > /dev/console
+    rpcclient "$ATOM_ARPING_IP" "/usr/bin/speedtest-client &"
+  else
+    # NodeJS speedtest client
+    echo "Executing run_speedtest.sh for xb6a" > /dev/console
+    rpcclient "$ATOM_ARPING_IP" "/etc/speedtest/run_speedtest.sh &"
+  fi
+elif [ "$BOX_TYPE" = XB6 ] && [ "$MODEL_NUM" = CGM4140COM ]
+then
+  if [ "x$ST_CLIENT_TYPE" = 'x1' ]
+  then
+    # C speedtest client
+    echo "Executing native c speedtest-client for xb6t" > /dev/console
+    /usr/bin/speedtest-client
+  else
+    # NodeJS speedtest client
+    echo "Executing run_speedtest.sh for xb6t" > /dev/console
+    sh /etc/speedtest/run_speedtest.sh
+  fi
+elif [ "$BOX_TYPE" = TCCBR ] && [ "$MODEL_NUM" = CGA4131COM ]
+then
+  if [ "x$ST_CLIENT_TYPE" = 'x1' ]
+  then
+    # C speedtest client
+    echo "Executing native c speedtest-client for CBR" > /dev/console
+    /usr/bin/speedtest-client
+  else
+    # NodeJS speedtest client
+    echo "Executing run_speedtest.sh for CBR" > /dev/console
+    sh /etc/speedtest/run_speedtest.sh
+  fi
+elif [ "$BOX_TYPE" = XB3 ] && [ "$MODEL_NUM" = DPC3941 ]
+then
+  if [ "x$ST_CLIENT_TYPE" = 'x1' ]
+  then
+    # C speedtest client
+    echo "Executing native c speedtest-client for xb3c" > /dev/console
+    rpcclient "$ATOM_ARPING_IP" "/usr/bin/speedtest-client &"
+  else
+    # NodeJS speedtest client
+    echo "Executing run_speedtest.sh for xb3c" > /dev/console
+    rpcclient "$ATOM_ARPING_IP" "/etc/speedtest/run_speedtest.sh &"
+  fi
+elif [ "$BOX_TYPE" = XB3 ] && [ "$MODEL_NUM" = DPC3941B ]
+then
+  if [ "x$ST_CLIENT_TYPE" = 'x1' ]
+  then
+    # C speedtest client
+    echo "Executing native c speedtest-client for xb3c" > /dev/console
+    rpcclient "$ATOM_ARPING_IP" "/usr/bin/speedtest-client &"
+  else
+    # NodeJS speedtest client
+    echo "Executing run_speedtest.sh for xb3c" > /dev/console
+    rpcclient "$ATOM_ARPING_IP" "/etc/speedtest/run_speedtest.sh &"
+  fi
+elif [ "$BOX_TYPE" = XF3 ]
 then
   echo "Executing run_speedtest.sh" > /dev/console
   sh /etc/speedtest/run_speedtest.sh
