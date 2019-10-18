8426b01f7645b30f8463998747712be7393d9197
narayanaswamy <Narayanaswamy_Ramaiyer@comcast.com>
2019-10-18 22:50:46 +0530

    INFRA-666:[Exercise] Self heal Reason for change: Updating code for memory load and script for resource monitor for better exercise experience Test : Build the code , set the threshold to a low value like 20, source self heal log and execute either of the binary Risks: Low
    Change-Id: I20b2e4cf3a568910e077e860879473ae0995110e

--- source/memory_load/memory_load.c
+++ source/memory_load/memory_load.c
@@ -1,23 +1,16 @@
-#include<stdio.h>
-
 #include<stdlib.h>
-
 #include<string.h>
-
+#include <unistd.h>
 int main()
-
 {
-
-        while(1)
-
-        {
-
-                char *ptr = malloc(100);
-
-                strcpy(ptr, "HelloWorld");
-
-        }
-
-        return 0;
-
+	while(1)
+	{
+		//Increased memory allocation size for faster memory fill up
+		char *ptr = (char*)malloc(100 * 1024);
+		memset( ptr, 0xF , 100*1024 );
+		//Reduced sleep timer for faster memory fill up
+		usleep( 1500 );
+	}
+	return 0;
 }
+
