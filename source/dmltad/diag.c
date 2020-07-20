71f920621994eb73713006b31a688804ae62f410
Anu Shaju Ainikkal <ashaju.contractor@libertyglobal.com>
2020-07-20 11:47:25 +0200

    add data model support for cancelling ping diagnostics ( RDKB-4467 )
    DESCRIPTION:
    Implement the Device.IP.Diagnostics.IPPing.DiagnosticsState to
    support "Canceled" option. If the Diagnostics state test is in
    progress, setting the parameter value to "Canceled" must result in
    the test being cancelled and the value of the parameter being set to
    "None".
    
    TESTING PERFORMED:
    1.Provision the gateway
    2.Enable ping test
      dmcli eRT setv Device.IP.Diagnostics.IPPing.Interface string erouter0
      dmcli eRT setv Device.IP.Diagnostics.IPPing.Host string 8.8.8.8
      dmcli eRT setv Device.IP.Diagnostics.IPPing.DiagnosticsState to "Requested"
    3.Change the DiagnosticsState value to Canceled
      dmcli eRT setv Device.IP.Diagnostics.IPPing.DiagnosticsState to "Canceled"
    4.Check Device.IP.Diagnostics.IPPing.DiagnosticsState, it should changed to "None"
    
    Signed-off-by: Anu Shaju Ainikkal(ashaju.contractor@libertyglobal.com)

--- source/dmltad/diag.c
+++ source/dmltad/diag.c
@@ -86,6 +86,8 @@ int commonSyseventGet(char* key, char* value, int valLen){
     return sysevent_get(commonSyseventFd, commonSyseventToken, key, value, valLen);
 }
 
+#define DIAG_RESULT_STR_LEN 16
+
 /* XXX: if there are more instances, we may use a dynamic list to 
  * handle these instances, or with dynamic load. */
 
@@ -621,6 +623,29 @@ diag_err_t diag_stop(diag_mode_t mode)
     if (diag->state != DIAG_ST_NONE)
         err = __diag_stop(diag);
     diag->state = DIAG_ST_NONE;
+
+    if (mode == DIAG_MD_PING)
+    {
+        char result[DIAG_RESULT_STR_LEN];
+        FILE *fp;
+
+        if ((fp = popen("cat /var/tmp/pinging.txt | grep from | wc -l", "r")) == NULL)
+            err = DIAG_ERR_INTERNAL;
+        else
+        {
+            if (fgets(result, sizeof(result), fp) == NULL) {
+                err = DIAG_ERR_OTHER;
+            }
+            else
+            {
+                diag->stat.u.ping.success = atoi(result);
+                fprintf(stderr, "%s: diag->stat.u.ping.success=%d \n", __FUNCTION__, diag->stat.u.ping.success);
+            }
+
+            pclose(fp);
+        }
+    }
+
     pthread_mutex_unlock(&diag->mutex);
 
     if (err != DIAG_ERR_OK)
