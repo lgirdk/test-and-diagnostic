4a2816e27c815a2d9e191e84f6337e3d498993fd
Anu Shaju Ainikkal <ashaju.contractor@libertyglobal.com>
2020-09-24 10:01:50 +0200

    ping and traceroute functionality on DSLite mode ( RDKB-4722 )
    BUG DESCRIPTION:
    
       Support needs to be added to Ping and Traceroute to ipv4
       addresses and domain names when in DS-Lite mode.
    
    FIX DESCRIPTION:
    
       Specifically using the interface option in the Ping and
       Traceroute command line to mention the brlan0 interface
       could resolve the problem.
    
    TESTING PERFORMED:
       -Verified Ping and Traceroute to IPv4 addresses, domain name strings
        when in DS-lite mode.
    
    Signed-off-by: Anu Shaju Ainikkal(ashaju.contractor@libertyglobal.com)
    
    ping and traceroute functionality on DSLite mode ( RDKB-4722 ) - fix interface names
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source/dmltad/cosa_ip_dml.c
+++ source/dmltad/cosa_ip_dml.c
@@ -1640,6 +1640,10 @@ IPPing_GetParamUlongValue
         *puLong = cfg.size;
     else if (strcmp(ParamName, "DSCP") == 0)
         *puLong = cfg.tos >> 2;
+    else if (strcmp(ParamName, "Interval") == 0)
+        *puLong = cfg.interval * 1000;
+    else if (strcmp(ParamName, "PingDNSQueryIPType") == 0)
+         *puLong = cfg.pingdnsquerytype;
     else if (strcmp(ParamName, "SuccessCount") == 0)
         *puLong = statis.u.ping.success;
     else if (strcmp(ParamName, "FailureCount") == 0)
@@ -1916,6 +1920,10 @@ IPPing_SetParamUlongValue
     }
     else if (strcmp(ParamName, "DSCP") == 0)
         cfg.tos = uValue << 2;
+    else if (strcmp(ParamName, "Interval") == 0)
+        cfg.interval = uValue / 1000;
+    else if (strcmp(ParamName, "PingDNSQueryIPType") == 0)
+        cfg.pingdnsquerytype = uValue;
     else
         return FALSE;
 
@@ -2366,6 +2374,8 @@ TraceRoute_GetParamUlongValue
         *puLong = cfg.size;
     else if (strcmp(ParamName, "DSCP") == 0)
         *puLong = cfg.tos >> 2;
+    else if (strcmp(ParamName, "TraceDNSQueryIPType") == 0)
+        *puLong = cfg.tracednsquerytype;
     else if (strcmp(ParamName, "MaxHopCount") == 0)
         *puLong = cfg.maxhop;
     else if (strcmp(ParamName, "ResponseTime") == 0)
@@ -2629,6 +2639,8 @@ TraceRoute_SetParamUlongValue
         cfg.size = uValue;
     else if (strcmp(ParamName, "DSCP") == 0)
         cfg.tos = uValue << 2;
+    else if (strcmp(ParamName, "TraceDNSQueryIPType") == 0)
+        cfg.tracednsquerytype = uValue;
     else if (strcmp(ParamName, "MaxHopCount") == 0)
     {
         cfg.maxhop = uValue;
