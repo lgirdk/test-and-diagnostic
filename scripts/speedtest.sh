8deadef9d9406b2385e2eeec1f67099fc4afec8b
karthikeyan shanmugam <karthikeyan_shanmugam@comcast.com>
2020-10-21 15:21:55 +0530

    RDKB-19277, ST2-10150 : Remove Node client support, clean up script
    Reason for change: Timestamp added for the log messages. NodeJS support removed for XB3,XB6.
    Test Procedure: Run the speedtest.sh script on XB3,XB6 device.
    Verify the timestamp in the log file /rdklogs/logs/speedtest-init.log
    Risks: None
    Signed-off-by: Karthik <karthikeyan_shanmugam@comcast.com>
    
    (cherry picked from commit 73a5c91800618246d2ad8715880475e0886f1836)
    (cherry picked from commit b63ca36d901a4b9171c4d9fd0b0d7034f207a815)
    Change-Id: I4d13cfd23c4b8d7e01332559e68b1290dc61c6d0

--- scripts/speedtest.sh
+++ scripts/speedtest.sh
@@ -19,68 +19,43 @@
 #######################################################################################
 
 #This script is used to enable / start the speedtest tool
+LOG_FILE=/rdklogs/logs/speedtest-init.log
 
-echo "Enabling / Starting speedtest..." > /dev/console
+echo "$(date +"[%Y-%m-%d %H:%M:%S]" ) Enabling / Starting speedtest..." >> $LOG_FILE
 . /etc/device.properties
 
-# Two implementations of the speedtest currently co-exist on most of our platforms - a Node.js client, and a C client.
+# C client implementation of the speedtest currently supported for our platforms.
+# Node.js client support removed for XB3 ,XB6 and XF3 devices.
   # If Device.IP.Diagnostics.X_RDKCENTRAL-COM_SpeedTest.ClientType exists, then:
   # Device.IP.Diagnostics.X_RDKCENTRAL-COM_SpeedTest.ClientType = 1 implies the C client should run.
-  # Device.IP.Diagnostics.X_RDKCENTRAL-COM_SpeedTest.ClientType = 2 implies the NodeJS client should run.
 ST_CLIENT_TYPE=`dmcli eRT getv Device.IP.Diagnostics.X_RDKCENTRAL-COM_SpeedTest.ClientType | awk '/value/ { print $5 }'`
 
-if [ "$BOX_TYPE" = XB3 ] && [ "$MODEL_NUM" = TG1682G ]
+if [ "x$ST_CLIENT_TYPE" = 'x1' ]
 then
-  if [ "x$ST_CLIENT_TYPE" = 'x1' ]
-  then
+   if [ "$BOX_TYPE" = XB3 ] && [ "$MODEL_NUM" = TG1682G ]
+   then
     # C speedtest client
-    echo "Executing native c speedtest-client for xb3a" > /dev/console
+    echo "$(date +"[%Y-%m-%d %H:%M:%S]" ) Executing speedtest-client-c for XB3A" >> $LOG_FILE
     rpcclient "$ATOM_ARPING_IP" "/usr/bin/speedtest-client &"
-  else
-    # NodeJS speedtest client
-    echo "Executing run_speedtest.sh for xb3a" > /dev/console
-    rpcclient "$ATOM_ARPING_IP" sh /etc/speedtest/run_speedtest.sh
-  fi
-elif [ "$BOX_TYPE" = XB6 ]
-then
-  if [ "x$ST_CLIENT_TYPE" = 'x1' ]
-  then
+   elif [ "$BOX_TYPE" = XB6 ]
+   then
     # C speedtest client
-    echo "Executing native c speedtest-client for xb6t/xb7t" > /dev/console
+    echo "$(date +"[%Y-%m-%d %H:%M:%S]" ) Executing speedtest-client-c for XB6/XB7/XB8" >> $LOG_FILE
     /usr/bin/speedtest-client
-  else
-    # NodeJS speedtest client
-    echo "Executing run_speedtest.sh for xb6t/xb7t" > /dev/console
-    sh /etc/speedtest/run_speedtest.sh
-  fi
-elif [ "$BOX_TYPE" = TCCBR ]
-then
-  if [ "x$ST_CLIENT_TYPE" = 'x1' ]
-  then
+   elif [ "$BOX_TYPE" = TCCBR ]
+   then
     # C speedtest client
-    echo "Executing native c speedtest-client for CBR" > /dev/console
+    echo "$(date +"[%Y-%m-%d %H:%M:%S]" ) Executing speedtest-client-c for CBR" >> $LOG_FILE
     /usr/bin/speedtest-client
-  else
-    # NodeJS speedtest client
-    echo "Executing run_speedtest.sh for CBR" > /dev/console
-    sh /etc/speedtest/run_speedtest.sh
-  fi
-elif [ "$BOX_TYPE" = XB3 ] && ( [ "$MODEL_NUM" = DPC3941 ] || [ "$MODEL_NUM" = DPC3941B ] )
-then
-  if [ "x$ST_CLIENT_TYPE" = 'x1' ]
-  then
+   elif [ "$BOX_TYPE" = XB3 ] && ( [ "$MODEL_NUM" = DPC3941 ] || [ "$MODEL_NUM" = DPC3941B ] )
+   then
     # C speedtest client
-    echo "Downloading and/or executing native c speedtest-client for xb3c" > /dev/console
+    echo "$(date +"[%Y-%m-%d %H:%M:%S]" ) Downloading and/or executing speedtest-client-c for XB3C" >> $LOG_FILE
     rpcclient "$ATOM_ARPING_IP" "sh /etc/measurement-client-download.sh &"
-  else
-    # NodeJS speedtest client
-    echo "Executing run_speedtest.sh for xb3c" > /dev/console
-    rpcclient "$ATOM_ARPING_IP" sh /etc/speedtest/run_speedtest.sh
-  fi
-elif [ "$BOX_TYPE" = XF3 ]
-then
-  echo "Executing run_speedtest.sh" > /dev/console
-  sh /etc/speedtest/run_speedtest.sh
+   else
+    # Unsupported speedtest client
+    echo "$(date +"[%Y-%m-%d %H:%M:%S]" ) Unsupported device model" >> $LOG_FILE
+   fi
 else
-  echo "Unsupported device model" > /dev/console
+  echo "$(date +"[%Y-%m-%d %H:%M:%S]" ) Unsupported client" >> $LOG_FILE
 fi
