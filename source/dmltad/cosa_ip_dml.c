d0fd8159d47bfc4d1df2e2df7ee78d359525a88c
Andre McCurdy <armccurdy@gmail.com>
2020-11-02 12:51:17 -0800

    support setting Device.IP.Diagnostics.IPPing Host and Interface to empty strings ( OFW-113 )
    "Host" and "Interface" have data type string(0:256) so setting to an
    empty string should be allowed.
    
    If Interface is empty, use "Device.IP.Interface.1" as the default.
    
    Verified that below dmcli commands succeed:
    
      dmcli eRT setv Device.IP.Diagnostics.IPPing.Interface string ""
      dmcli eRT setv Device.IP.Diagnostics.IPPing.Host string ""
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source/dmltad/cosa_ip_dml.c
+++ source/dmltad/cosa_ip_dml.c
@@ -1896,9 +1896,19 @@ IPPing_SetParamStringValue
     if (diag_getcfg(DIAG_MD_PING, &cfg) != DIAG_ERR_OK)
         return FALSE;
 
-    /* fail if pString is NULL, an empty string or contains <space> or any of <>&,'"| */
-    if (AnscValidStringCheck(pString) != TRUE)
+    // "Host" and "Interface" can be empty strings. i.e data type string(256)
+    if( AnscEqualString(ParamName, "Host", TRUE) || AnscEqualString(ParamName, "Interface", TRUE) )
+    {
+      if (!pString)
+      {
+        fprintf(stderr, "\n %s: %d ParamName:%s value is NULL  \n", __FUNCTION__,__LINE__,ParamName);
         return FALSE;
+      }
+    }
+    else if (AnscValidStringCheck(pString) != TRUE) /* fail if pString is NULL, an empty string or contains <space> or any of <>&,'"| */
+    {
+      return FALSE;
+    }
 
     if (strcmp(ParamName, "Interface") == 0)
     {
@@ -1922,7 +1932,15 @@ IPPing_SetParamStringValue
             parameterValStruct_t    ParamVal;
             int                     size = sizeof(cfg.ifname);
 
-            rc = sprintf_s(IfNameParamName, sizeof(IfNameParamName), "%s.Name", cfg.Interface);
+            if(strlen(cfg.Interface))
+            {
+                rc = sprintf_s(IfNameParamName, sizeof(IfNameParamName), "%s.Name", cfg.Interface);
+            }
+            else
+            {
+                /* If an empty string is specified, use "Device.IP.Interface.1" as the interface */
+                rc = sprintf_s(IfNameParamName, sizeof(IfNameParamName), "%s.Name", "Device.IP.Interface.1");
+            }
             if(rc < EOK)
             {
                 ERR_CHK(rc);
@@ -1948,7 +1966,7 @@ IPPing_SetParamStringValue
     else if (strcmp(ParamName, "Host") == 0)
     {
 		ANSC_STATUS             ret;
-		char wrapped_host[64]={0};
+		char wrapped_host[256]={0}; //Host size is string(256)
         ret=CosaDmlInputValidation(pString, wrapped_host, AnscSizeOfString(pString), sizeof( wrapped_host ));
 		if(ANSC_STATUS_SUCCESS != ret)
 			return FALSE;
