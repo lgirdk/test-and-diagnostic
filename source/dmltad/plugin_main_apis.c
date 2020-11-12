a48d535b28c344e886f7128b34e30aab93dc667d
Anu Shaju Ainikkal <ashaju.contractor@libertyglobal.com>
2020-11-12 15:27:26 +0100

    Telemetry 2.0 - Datamodel implementation ( RDKB-4874 )
    Added Datamodel implementation of Telemetry prameters except Enable,
    DCMConfigForceDownload.
    
    TESTING PERFORMED:
    
      1. set Telemetry syscfg parameters using syscfg set
      2. GPV,SPV implemented Telemetry parameters
    
    Signed-off-by: Anu Shaju Ainikkal(ashaju.contractor@libertyglobal.com)
    
    minor Makefile cleanups ( + remove .h files from libdmltad_la_SOURCES )
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>
    
    Telemetry 2.0 - Device.X_LGI-COM_Telemetry. datamodel tree has broken ( RDKB-5082 )
    
    BUG DESCRIPTION:
    
      - GPV of Device.X_LGI-COM_Telemetry. not listing all parameters in the tree.
    
    FIX DESCRIPTION:
    
        The return type of folowing fuctions changed to ANSC_STATUS from BOOL
        -Telemetry_GetParamStringValue
        -DcmDownloadStatus_GetParamStringValue
        -UploadStatus_GetParamStringValue
    
    TESTING PERFORMED:
        GPV the following parameters.
        -dmcli eRT getv Device.X_LGI-COM_Telemetry.
        -dmcli eRT getv Device.X_LGI-COM_Telemetry.DcmDownloadStatus.
        -dmcli eRT getv Device.X_LGI-COM_Telemetry.UploadStatus.
        -dmcli eRT getv Device.X_LGI-COM_Telemetry.DcmRetryConfig.
    
    Signed-off-by: Anu Shaju Ainikkal(ashaju.contractor@libertyglobal.com)

--- source/dmltad/plugin_main_apis.c
+++ source/dmltad/plugin_main_apis.c
@@ -232,6 +232,9 @@ CosaBackEndManagerInitialize
     pMyObject->hInterfaceStack = (ANSC_HANDLE)CosaIFStackCreate();
     pMyObject->hPPP           = (ANSC_HANDLE)CosaPPPCreate();
     */
+    //Telemetry Changes
+    pMyObject->hTelemetry     = (ANSC_HANDLE)CosatelemetryCreate();
+    AnscTraceWarning(("  CosatelemetryCreate done!\n"));
 
     return returnStatus;
 }
@@ -285,6 +288,11 @@ CosaBackEndManagerRemove
     if ( pMyObject->hDiag )
     {
         CosaDiagRemove((ANSC_HANDLE)pMyObject->hDiag);
+    }
+    // Telemetry Code
+    if ( pMyObject->hTelemetry )
+    {
+        CosatelemetryRemove((ANSC_HANDLE)pMyObject->hTelemetry);
     }
 /*
     if ( pMyObject->hDeviceInfo )
