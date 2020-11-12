9b11338808df575b56be8f84f896bad4f450fe6c
Anu Shaju Ainikkal <ashaju.contractor@libertyglobal.com>
2020-11-12 15:27:26 +0100

    Telemetry 2.0 - Datamodel implementation ( RDKB-4874 )
    Added Datamodel implementation of Telemetry prameters except Enable,
    DCMConfigForceDownload.
    
    TESTING PERFORMED:
    
      1. set Telemetry syscfg parameters using syscfg set
      2. GPV,SPV implemented Telemetry parameters
    
    Signed-off-by: Anu Shaju Ainikkal(ashaju.contractor@libertyglobal.com)
    
    minor Makefile cleanups ( + remove .h files from libdmltad_la_SOURCES )
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>
    
    Telemetry 2.0 - Device.X_LGI-COM_Telemetry. datamodel tree has broken ( RDKB-5082 )
    
    BUG DESCRIPTION:
    
      - GPV of Device.X_LGI-COM_Telemetry. not listing all parameters in the tree.
    
    FIX DESCRIPTION:
    
        The return type of folowing fuctions changed to ANSC_STATUS from BOOL
        -Telemetry_GetParamStringValue
        -DcmDownloadStatus_GetParamStringValue
        -UploadStatus_GetParamStringValue
    
    TESTING PERFORMED:
        GPV the following parameters.
        -dmcli eRT getv Device.X_LGI-COM_Telemetry.
        -dmcli eRT getv Device.X_LGI-COM_Telemetry.DcmDownloadStatus.
        -dmcli eRT getv Device.X_LGI-COM_Telemetry.UploadStatus.
        -dmcli eRT getv Device.X_LGI-COM_Telemetry.DcmRetryConfig.
    
    Signed-off-by: Anu Shaju Ainikkal(ashaju.contractor@libertyglobal.com)
    
    Telemetry 2.0 - Datamodel implementation ( RDKB-4932 )
    
    DESCRIPTION:
    Implemented Device.X_LGI-COM_Telemetry.Enable data model.
    
    TESTING PERFORMED:
    1. Set Device.X_LGI-COM_Telemetry.Enable as true.
    2. Telemetry agent(telemetry2_0) process should start to run.
    3. Set Device.X_LGI-COM_Telemetry.Enable as false.
    4. Telemetry agent(telemetry2_0) process should get terminated.
    
    Signed-off-by: Cibil <cpankiras.contractor@libertyglobal.com>
    
    Telemetry Data Model Implementation - Device.X_LGI-COM_Telemetry.DCMConfigForceDownload ( RDKB-4933 )
    
    New Feature : Introduced ForceDownload parameter to download dcm configuration file and reload the
    telemetry service with new configuration file
    
    Testing Performed :
    Performed functionality testing using Telemetry DML
    dmcli eRT setv Device.X_LGI-COM_Telemetry.Enable bool false
    dmcli eRT setv Device.X_LGI-COM_Telemetry.DCMConfigForceDownload bool true
    dmcli eRT setv Device.X_LGI-COM_Telemetry.DCMConfigForceDownload bool false
    
    Signed-off-by: sarveswaran.n <sanagarajan.contractor@libertyglobal.com>
    
    cleanup telemetry state enum declaration + make tele_state variable static
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>
    
    fix telemetry crash after force download ( OFW-2199 F3896SI-614 )
    
    BUG DESCRIPTION:
    
    After setting force download the telemetry process get crashed.
    
    FIX DESCRIPTION:
    
    Changed the signal kill value, so that the process is reloading instead of terminating.
    
    TESTING PERFORMED:
    
    1.Enable telemetry
    2. check "ps aux | grep tele"
    3. SPV DCMConfigForceDownload to true
    4. check "ps aux | grep tele"
    5. Telemetry process is running without any crash
    
    Signed-off-by: Saravanakumar Karthikeyan <skarthikeyan.contractor@libertyglobal.com>
    
    RDKB-5012 - Implement Device.X_LGI-COM_Telemetry.DCMConfigFileURL parameter
    
    Testing Performed :
    dmcli eRT setv Device.X_LGI-COM_Telemetry.DCMConfigFileURL string http://172.30.117.124/ssl/DCMresponse-test.json
    dmcli eRT setv Device.X_LGI-COM_Telemetry.DCMConfigFileURL string https://firmware-lg-pl.prod.cdn.dmdsdp.com/DCMresponse.txt
    
    Signed-off-by: sarveswaran.n <sanagarajan.contractor@libertyglobal.com>

---
+++ source/dmltad/cosa_telemetry_internal.c
@@ -0,0 +1,242 @@
+/*********************************************************************************
+ * Copyright 2019 Liberty Global B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+  *********************************************************************************/
+
+
+/**************************************************************************
+    module: cosa_telemetry_internal.c
+    For COSA Data Model Library Development
+    description:
+    This file implementes Enable /Disable telemetry service
+    environment:
+    platform independent
+**************************************************************************/
+
+#include <syscfg/syscfg.h>
+
+#include "cosa_telemetry_internal.h"
+#include "cosa_telemetry_dml.h"
+#include "plugin_main_apis.h"
+
+
+/**********************************************************************
+Caller:         Owner of the object
+Prototype:      ANSC_HANDLE CosatelemetryCreate();
+Description:    This function constructs cosa telemetry object and return handle.
+Argument:
+Return:         Newly created telemetry object.
+**********************************************************************/
+ANSC_HANDLE CosatelemetryCreate(void)
+{
+    CcspTraceDebug(("%s Entered\n", __FUNCTION__));
+
+    PCOSA_DATAMODEL_TELEMETRY       pMyObject    = (PCOSA_DATAMODEL_TELEMETRY)NULL;
+    /*
+     * We create object by first allocating memory for holding the variables and member functions.
+     */
+    pMyObject = (PCOSA_DATAMODEL_TELEMETRY)AnscAllocateMemory(sizeof(COSA_DATAMODEL_TELEMETRY));
+
+    if (!pMyObject)
+    {
+        CcspTraceError(("%s exit ERROR\n", __FUNCTION__));
+        return (ANSC_HANDLE)NULL;
+    }
+
+    pMyObject->Oid               = COSA_DATAMODEL_TELEMETRY_OID;
+    pMyObject->Create            = CosatelemetryCreate;
+    pMyObject->Remove            = CosatelemetryRemove;
+    pMyObject->Initialize        = CosatelemetryInitialize;
+
+    pMyObject->Initialize((ANSC_HANDLE)pMyObject);
+    return (ANSC_HANDLE)pMyObject;
+}
+
+PDML_DCM_DOWNLOAD_STATUS DmlGetDcmStatus(ANSC_HANDLE hThisObject)
+{
+    char intBuf[12];
+
+    CcspTraceDebug(("%s Entered\n", __FUNCTION__));
+
+    PCOSA_DATAMODEL_TELEMETRY pMyObject = (PCOSA_DATAMODEL_TELEMETRY)hThisObject;
+    PDML_DCM_DOWNLOAD_STATUS  pDcmStatus = (PDML_DCM_DOWNLOAD_STATUS)NULL;
+
+    pDcmStatus = (PDML_DCM_DOWNLOAD_STATUS)AnscAllocateMemory(sizeof(DML_DCM_DOWNLOAD_STATUS));
+    if (!pDcmStatus)
+    {
+        CcspTraceError(("\n %s DCM Download Status allocation failed\n", __FUNCTION__));
+        return NULL;
+    }
+
+    syscfg_get(NULL, "dcm_lastSuccessTimestamp", pDcmStatus->LastSuccessTimestamp, sizeof(pDcmStatus->LastSuccessTimestamp));
+    syscfg_get(NULL, "dcm_lastAttemptTimestamp", pDcmStatus->LastAttemptTimestamp, sizeof(pDcmStatus->LastAttemptTimestamp));
+    syscfg_get(NULL, "dcm_httpStatusString", pDcmStatus->HTTPStatusString, sizeof(pDcmStatus->HTTPStatusString));
+
+    syscfg_get(NULL, "dcm_httpStatus", intBuf, sizeof(intBuf));
+    pDcmStatus->HTTPStatus = strtol(intBuf, NULL, 10);
+
+    syscfg_get(NULL, "dcm_attemptCount", intBuf, sizeof(intBuf));
+    pDcmStatus->AttemptCount = strtol(intBuf, NULL, 10);
+
+    return pDcmStatus;
+}
+
+PDML_ODH_UPLOAD_STATUS DmlGetUploadStatus(ANSC_HANDLE hThisObject)
+{
+    char intBuf[12];
+
+    CcspTraceDebug(("%s Entered\n", __FUNCTION__));
+
+    PCOSA_DATAMODEL_TELEMETRY pMyObject = (PCOSA_DATAMODEL_TELEMETRY)hThisObject;
+    PDML_ODH_UPLOAD_STATUS    pUploadStatus = (PDML_ODH_UPLOAD_STATUS)NULL;
+
+    pUploadStatus = (PDML_ODH_UPLOAD_STATUS)AnscAllocateMemory(sizeof(DML_ODH_UPLOAD_STATUS));
+    if (!pUploadStatus)
+    {
+        CcspTraceError(("\n %s DCA upload status allocation failed\n", __FUNCTION__));
+        return NULL;
+    }
+
+    syscfg_get(NULL, "upload_lastSuccessTimestamp", pUploadStatus->LastSuccessTimestamp, sizeof(pUploadStatus->LastSuccessTimestamp));
+    syscfg_get(NULL, "upload_lastAttemptTimestamp", pUploadStatus->LastAttemptTimestamp, sizeof(pUploadStatus->LastAttemptTimestamp));
+    syscfg_get(NULL, "upload_httpStatusString", pUploadStatus->HTTPStatusString, sizeof(pUploadStatus->HTTPStatusString));
+
+    syscfg_get( NULL, "upload_httpStatus", intBuf, sizeof(intBuf));
+    pUploadStatus->HTTPStatus = strtol(intBuf, NULL, 10);
+
+    syscfg_get(NULL, "upload_attemptCount", intBuf, sizeof(intBuf));
+    pUploadStatus->AttemptCount = strtol(intBuf, NULL, 10);
+
+    return pUploadStatus;
+}
+
+PDML_DCM_RETRY_CONFIG DmlGetRetryCfg(ANSC_HANDLE hThisObject)
+{
+    char intBuf[12];
+
+    CcspTraceDebug(("%s Entered\n", __FUNCTION__));
+
+    PCOSA_DATAMODEL_TELEMETRY pMyObject = (PCOSA_DATAMODEL_TELEMETRY)hThisObject;
+    PDML_DCM_RETRY_CONFIG     pRetryConfig = (PDML_DCM_RETRY_CONFIG)NULL;
+
+    pRetryConfig = (PDML_DCM_RETRY_CONFIG)AnscAllocateMemory(sizeof(DML_DCM_RETRY_CONFIG));
+    if (!pRetryConfig)
+    {
+        CcspTraceError(("\n %s DCM retry config allocation failed\n", __FUNCTION__));
+        return NULL;
+    }
+
+    syscfg_get(NULL, "dcm_retry_attemptInterval", intBuf, sizeof(intBuf));
+    pRetryConfig->AttemptInterval = strtol(intBuf, NULL, 10);
+
+    syscfg_get( NULL, "dcm_retry_maxAttempts", intBuf, sizeof(intBuf));
+    pRetryConfig->MaxAttempts = strtol(intBuf, NULL, 10);
+
+    return pRetryConfig;
+}
+
+/**********************************************************************
+Caller:         Owner of the object
+Prototype:      ANSC_HANDLE CosatelemetryInitialize ();
+Description:    This function Initializes the datamodel object and call the services accordingly.
+Argument:
+Return:         Returns the ANSC_STATUS.
+
+**********************************************************************/
+ANSC_STATUS CosatelemetryInitialize(ANSC_HANDLE hThisObject)
+{
+    ANSC_STATUS                     returnStatus        = ANSC_STATUS_SUCCESS;
+    PCOSA_DATAMODEL_TELEMETRY       pMyObject           = (PCOSA_DATAMODEL_TELEMETRY)hThisObject;
+    CcspTraceDebug(("%s Entered\n", __FUNCTION__));
+    returnStatus = CosaDmlTelemetryInit((ANSC_HANDLE)pMyObject);
+
+    if (returnStatus != ANSC_STATUS_SUCCESS)
+    {
+        CcspTraceError(("%s exit ERROR\n", __FUNCTION__));
+        return returnStatus;
+    }
+
+    pMyObject->pDcmStatus = DmlGetDcmStatus((ANSC_HANDLE)pMyObject);
+    pMyObject->pUploadStatus = DmlGetUploadStatus((ANSC_HANDLE)pMyObject);
+    pMyObject->pRetryConfig = DmlGetRetryCfg((ANSC_HANDLE)pMyObject);
+
+    return returnStatus;
+}
+
+/**********************************************************************
+Caller:         Owner of the object
+Prototype:      ANSC_HANDLE CosatelemetryRemove ();
+Description:    This function deletes the momory of the Telemetry Object.
+Argument:
+Return:         Returns the ANSC_STATUS.
+
+**********************************************************************/
+ANSC_STATUS CosatelemetryRemove ( ANSC_HANDLE hThisObject )
+{
+    ANSC_STATUS                     returnStatus = ANSC_STATUS_SUCCESS;
+    PCOSA_DATAMODEL_TELEMETRY       pMyObject    = (PCOSA_DATAMODEL_TELEMETRY)hThisObject;
+
+    CcspTraceDebug(("%s Entered\n", __FUNCTION__));
+
+    if (pMyObject)
+    {
+        if(pMyObject->pDcmStatus )
+        {
+            AnscFreeMemory(pMyObject->pDcmStatus);
+            pMyObject->pDcmStatus = NULL;
+        }
+        if (pMyObject->pUploadStatus)
+        {
+            AnscFreeMemory(pMyObject->pUploadStatus);
+            pMyObject->pUploadStatus = NULL;
+        }
+        if (pMyObject->pRetryConfig)
+        {
+            AnscFreeMemory(pMyObject->pRetryConfig);
+            pMyObject->pRetryConfig = NULL;
+        }
+
+        /* Remove self */
+        AnscFreeMemory((ANSC_HANDLE)pMyObject);
+    }
+
+    return returnStatus;
+}
+
+/**********************************************************************
+Caller:         Owner of the object
+Prototype:      ANSC_HANDLE CosaDmlTelemetryInit ();
+Description:    This function Initializes the objects for Telemetry and run the services accordingly..
+Argument:
+Return:         Returns the ANSC_STATUS_SUCCESS.
+
+**********************************************************************/
+ANSC_STATUS CosaDmlTelemetryInit(ANSC_HANDLE hThisObject)
+{
+    char buf[8];
+    PCOSA_DATAMODEL_TELEMETRY pMyObject = (PCOSA_DATAMODEL_TELEMETRY)hThisObject;
+
+    CcspTraceDebug(("%s Entered\n", __FUNCTION__));
+
+    syscfg_get(NULL, "telemetry_enable", buf, sizeof(buf));
+    pMyObject->Enable = (strcmp(buf, "true") == 0) ? TRUE : FALSE;
+
+    pMyObject->DCMConfigForceDownload = FALSE;
+
+    syscfg_get(NULL, "UploadRepositoryURL", pMyObject->UploadRepositoryURL, sizeof(pMyObject->UploadRepositoryURL));
+
+    syscfg_get(NULL, "T2ConfigURL", pMyObject->DCMConfigFileURL, sizeof(pMyObject->DCMConfigFileURL));
+
+    return ANSC_STATUS_SUCCESS;
+}
