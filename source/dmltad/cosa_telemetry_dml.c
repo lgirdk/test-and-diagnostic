8553f8bc349841c5415dca2cd363121dbcbf2c41
sarveswaran.n <sanagarajan.contractor@libertyglobal.com>
2020-11-26 06:06:13 +0100

    Telemetry Data Model Implementation - Device.X_LGI-COM_Telemetry.DCMConfigForceDownload ( RDKB-4933 )
    New Feature : Introduced ForceDownload parameter to download dcm configuration file and reload the
    telemetry service with new configuration file
    
    Testing Performed :
    Performed functionality testing using Telemetry DML
    dmcli eRT setv Device.X_LGI-COM_Telemetry.Enable bool false
    dmcli eRT setv Device.X_LGI-COM_Telemetry.DCMConfigForceDownload bool true
    dmcli eRT setv Device.X_LGI-COM_Telemetry.DCMConfigForceDownload bool false
    
    Signed-off-by: sarveswaran.n <sanagarajan.contractor@libertyglobal.com>
    
    cleanup telemetry state enum declaration + make tele_state variable static
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>
    
    fix telemetry crash after force download ( OFW-2199 F3896SI-614 )
    
    BUG DESCRIPTION:
    
    After setting force download the telemetry process get crashed.
    
    FIX DESCRIPTION:
    
    Changed the signal kill value, so that the process is reloading instead of terminating.
    
    TESTING PERFORMED:
    
    1.Enable telemetry
    2. check "ps aux | grep tele"
    3. SPV DCMConfigForceDownload to true
    4. check "ps aux | grep tele"
    5. Telemetry process is running without any crash
    
    Signed-off-by: Saravanakumar Karthikeyan <skarthikeyan.contractor@libertyglobal.com>

--- source/dmltad/cosa_telemetry_dml.c
+++ source/dmltad/cosa_telemetry_dml.c
@@ -21,7 +21,16 @@
 #include "cosa_telemetry_internal.h"
 #include "plugin_main_apis.h"
 
+typedef enum {
+    TELE_ST_NONE,
+    TELE_ST_START,
+    TELE_ST_STOP,
+    TELE_ST_RELOAD,
+}
+telemetry_state_t;
+
 static BOOL start_telemetry_service = FALSE;
+static telemetry_state_t tele_state = TELE_ST_NONE;
 
 /***********************************************************************
  APIs for Object:
@@ -37,10 +46,16 @@ Telemetry_GetParamBoolValue(ANSC_HANDLE hInsContext, char* ParamName, BOOL* bVal
 {
     CcspTraceDebug(("%s: Entered\n", __FUNCTION__));
     PCOSA_DATAMODEL_TELEMETRY pMyObject = (PCOSA_DATAMODEL_TELEMETRY)g_pCosaBEManager->hTelemetry;
-    if (AnscEqualString(ParamName, "Enable", TRUE))
+    if (strcmp(ParamName, "Enable") == 0)
     {
         CcspTraceDebug(("%s: Existing value for Telemetry.Enable is: %d\n", __FUNCTION__, pMyObject->Enable));
         *bValue =  pMyObject->Enable;
+        return TRUE;
+    }
+    else if (strcmp(ParamName, "DCMConfigForceDownload") == 0)
+    {
+        CcspTraceDebug(("%s: Existing value for Telemetry.DCMConfigForceDownload is: %d\n", __FUNCTION__, pMyObject->DCMConfigForceDownload));
+        *bValue = pMyObject->DCMConfigForceDownload;
         return TRUE;
     }
     else
@@ -55,7 +70,7 @@ Telemetry_SetParamBoolValue(ANSC_HANDLE hInsContext, char* ParamName, BOOL bValu
 {
     CcspTraceDebug(("%s: Entered\n", __FUNCTION__));
     PCOSA_DATAMODEL_TELEMETRY pMyObject = (PCOSA_DATAMODEL_TELEMETRY)g_pCosaBEManager->hTelemetry;
-    if (AnscEqualString(ParamName, "Enable", TRUE))
+    if (strcmp(ParamName, "Enable") == 0)
     {
         CcspTraceDebug(("%s Telemetry Enable Set Value is %d: and the existing value is %d\n", __FUNCTION__, bValue, pMyObject->Enable));
         if (pMyObject->Enable == bValue)
@@ -82,18 +97,28 @@ Telemetry_SetParamBoolValue(ANSC_HANDLE hInsContext, char* ParamName, BOOL bValu
         {
             if (bValue == TRUE)
             {
-                start_telemetry_service = TRUE;
+                tele_state = TELE_ST_START;
             }
             else
             {
-                CcspTraceInfo(("%s: Stop Telemetry Service, removing the cronjob (autodownload_dcmconfig.sh) from the cron table\n", __FUNCTION__));
-                system("crontab -l | grep -v 'autodownload_dcmconfig.sh' | crontab -");
-                system("killall -9 telemetry2_0");
+                tele_state = TELE_ST_STOP;
             }
         }
         pMyObject->Enable = bValue;
         CcspTraceDebug(("%s: Telemetry Enable Set Value is %d: and the value after set is %d\n", __FUNCTION__, bValue, pMyObject->Enable));
 
+        return TRUE;
+    }
+    else if (strcmp(ParamName, "DCMConfigForceDownload") == 0)
+    {
+        if (pMyObject->Enable == TRUE)  // Reload the service only if the Telemetry feature is enabled
+        {
+            if (bValue == TRUE)
+            {
+               tele_state = TELE_ST_RELOAD;
+            }
+        }
+        pMyObject->DCMConfigForceDownload = FALSE;
         return TRUE;
     }
     else
@@ -168,13 +193,31 @@ Telemetry_Commit
 {
     CcspTraceDebug(("%s: Entered\n", __FUNCTION__));
     PCOSA_DATAMODEL_TELEMETRY pMyObject = (PCOSA_DATAMODEL_TELEMETRY)g_pCosaBEManager->hTelemetry;
-    if ((pMyObject->Enable == TRUE) && (start_telemetry_service == TRUE))
-    {
-        CcspTraceInfo(("%s: Telemetry is enabled, downloading DCMConfigFile\n", __FUNCTION__));
-        CcspTraceInfo(("%s: Starting  Telemetry service\n", __FUNCTION__));
-        system("/usr/bin/telemetry2_0 &");
-    }
-    start_telemetry_service = FALSE;
+    switch(tele_state)
+        {
+	    case TELE_ST_START:
+            {
+                CcspTraceInfo(("%s: Telemetry is enabled, downloading DCMConfigFile\n", __FUNCTION__));
+                CcspTraceInfo(("%s: Starting  Telemetry service\n", __FUNCTION__));
+                system("/usr/bin/telemetry2_0 &");
+                break;
+            }
+	    case TELE_ST_STOP:
+            {
+                CcspTraceInfo(("%s: Telemetry is disabled \n", __FUNCTION__));
+                CcspTraceInfo(("%s: Stop Telemetry Service, removing the cronjob (autodownload_dcmconfig.sh) from the cron table\n", __FUNCTION__));
+                system("crontab -l | grep -v 'autodownload_dcmconfig.sh' | crontab -");
+                system("killall -9 telemetry2_0");
+                break;
+            }
+	    case TELE_ST_RELOAD :
+            {
+                CcspTraceInfo(("%s: Reloading Telemetry service with new configuration file \n", __FUNCTION__));
+                system("killall -12 telemetry2_0");
+                break;
+            }
+       }
+    tele_state = TELE_ST_NONE;
     return TRUE;
 }
 
