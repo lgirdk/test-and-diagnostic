60ea2ceb45c37e7fd885584dbbf51333a2a63925
sarveswaran.n <sanagarajan.contractor@libertyglobal.com>
2020-11-30 09:56:40 +0100

    RDKB-5012 - Implement Device.X_LGI-COM_Telemetry.DCMConfigFileURL parameter
    Testing Performed :
    dmcli eRT setv Device.X_LGI-COM_Telemetry.DCMConfigFileURL string http://172.30.117.124/ssl/DCMresponse-test.json
    dmcli eRT setv Device.X_LGI-COM_Telemetry.DCMConfigFileURL string https://firmware-lg-pl.prod.cdn.dmdsdp.com/DCMresponse.txt
    
    Signed-off-by: sarveswaran.n <sanagarajan.contractor@libertyglobal.com>

--- source/dmltad/cosa_telemetry_internal.c
+++ source/dmltad/cosa_telemetry_internal.c
@@ -236,5 +236,7 @@ ANSC_STATUS CosaDmlTelemetryInit(ANSC_HANDLE hThisObject)
 
     syscfg_get(NULL, "UploadRepositoryURL", pMyObject->UploadRepositoryURL, sizeof(pMyObject->UploadRepositoryURL));
 
+    syscfg_get(NULL, "T2ConfigURL", pMyObject->DCMConfigFileURL, sizeof(pMyObject->DCMConfigFileURL));
+
     return ANSC_STATUS_SUCCESS;
 }
