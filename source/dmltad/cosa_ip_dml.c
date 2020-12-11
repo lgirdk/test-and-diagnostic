2289c640e63af6a76e11c6962edee909278a42ca
Andre McCurdy <armccurdy@gmail.com>
2020-12-10 20:37:45 -0800

    update CosaDmlInputValidation() to validate hostname only, not add quotes too
    Adding quotes should be done at the time the hostname is used, not
    as part of validating it.
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source/dmltad/cosa_ip_dml.c
+++ source/dmltad/cosa_ip_dml.c
@@ -1948,13 +1948,13 @@ IPPing_SetParamStringValue
     }
     else if (strcmp(ParamName, "Host") == 0)
     {
-		ANSC_STATUS             ret;
-		char wrapped_host[256]={0}; //Host size is string(256)
-        ret=CosaDmlInputValidation(pString, wrapped_host, AnscSizeOfString(pString), sizeof( wrapped_host ));
-		if(ANSC_STATUS_SUCCESS != ret)
-			return FALSE;
-		
-        snprintf(cfg.host, sizeof(cfg.host), "%s", wrapped_host);
+        ANSC_STATUS ret;
+
+        ret = CosaDmlInputValidation(pString, sizeof(cfg.host));
+        if (ret != ANSC_STATUS_SUCCESS)
+            return FALSE;
+
+        snprintf(cfg.host, sizeof(cfg.host), "%s", pString);
     }
     else
         return FALSE;
@@ -2651,13 +2651,13 @@ TraceRoute_SetParamStringValue
     }
     else if (strcmp(ParamName, "Host") == 0)
     {
-		ANSC_STATUS             ret;
-		char wrapped_host[256]={0};//Host size is string(256)
-		ret=CosaDmlInputValidation(pString, wrapped_host, AnscSizeOfString(pString), sizeof( wrapped_host ));
-		if(ANSC_STATUS_SUCCESS != ret)
-			return FALSE;
+        ANSC_STATUS ret;
 
-        snprintf(cfg.host, sizeof(cfg.host), "%s", wrapped_host);
+        ret = CosaDmlInputValidation(pString, sizeof(cfg.host));
+        if (ret != ANSC_STATUS_SUCCESS)
+            return FALSE;
+
+        snprintf(cfg.host, sizeof(cfg.host), "%s", pString);
     }
     else
         return FALSE;
