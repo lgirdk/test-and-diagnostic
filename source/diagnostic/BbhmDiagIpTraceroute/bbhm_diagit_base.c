e13c7d29b2bfb6a44426cb474eaad3ba8a8b6282
Arundhati Ghose <aghose.contractor@libertyglobal.com>
2021-03-05 13:13:48 +0100

    add support for Device.IP.Diagnostics.TraceRoute.X_LGI-COM_BasePort ( OFW-531 )
    Bug Description:
    Device.IP.Diagnostics.TraceRoute.X_LGI-COM_BasePort is not supported
    
    Fix Description:
    Introduced a new DML parameter Device.IP.Diagnostics.TraceRoute.X_LGI-COM_BasePort in Test-and-Diagnostic component.
    
    Test Steps:
     1.Load the DUT with the image containing the changes and make sure it is online on ACS
     2.GPV of Device.IP.Diagnostics.TraceRoute.X_LGI-COM_BasePort was successful.
     3.SPV of Device.IP.Diagnostics.TraceRoute.X_LGI-COM_BasePort was successful.
    
    Signed-off-by: Arundhati <aghose.contractor@libertyglobal.com>
    
    update Traceroute X_LGI-COM_BasePort range from 0..65535 -> 1..65535
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source/diagnostic/BbhmDiagIpTraceroute/bbhm_diagit_base.c
+++ source/diagnostic/BbhmDiagIpTraceroute/bbhm_diagit_base.c
@@ -417,6 +417,8 @@ BbhmDiagitInitialize
     pMyObject->SetDstIp                     = BbhmDiagitSetDstIp;
     pMyObject->GetNumPkts                   = BbhmDiagitGetNumPkts;
     pMyObject->SetNumPkts                   = BbhmDiagitSetNumPkts;
+    pMyObject->GetBasePort                  = BbhmDiagitGetBasePort;
+    pMyObject->SetBasePort                  = BbhmDiagitSetBasePort;
     pMyObject->GetPktSize                   = BbhmDiagitGetPktSize;
     pMyObject->SetPktSize                   = BbhmDiagitSetPktSize;
     pMyObject->GetTimeBetween               = BbhmDiagitGetTimeBetween;
