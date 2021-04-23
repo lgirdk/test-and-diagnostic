a78695f5cfd23916655314cdd0492adb0c14a1de
mkamat <mkamat@libertyglobal.com>
2021-04-23 10:06:04 -0700

    replace rpcclient with rpcclient2

--- scripts/speedtest.sh
+++ scripts/speedtest.sh
@@ -36,7 +36,7 @@ then
    then
     # C speedtest client
     echo "$(date +"[%Y-%m-%d %H:%M:%S]" ) Executing speedtest-client-c for XB3A" >> $LOG_FILE
-    rpcclient "$ATOM_ARPING_IP" "/usr/bin/speedtest-client &"
+    rpcclient2 "/usr/bin/speedtest-client &"
    elif [ "$BOX_TYPE" = XB6 ]
    then
     # C speedtest client
@@ -51,7 +51,7 @@ then
    then
     # C speedtest client
     echo "$(date +"[%Y-%m-%d %H:%M:%S]" ) Downloading and/or executing speedtest-client-c for XB3C" >> $LOG_FILE
-    rpcclient "$ATOM_ARPING_IP" "sh /etc/measurement-client-download.sh &"
+    rpcclient2 "sh /etc/measurement-client-download.sh &"
    else
     # Unsupported speedtest client
     echo "$(date +"[%Y-%m-%d %H:%M:%S]" ) Unsupported device model" >> $LOG_FILE
