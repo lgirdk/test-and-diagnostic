4193c7e83c16eae4eefc7583bd67358c0e2f56ad
Rob Vogelaar <revogelaar@gmail.com>
2021-05-18 16:48:08 -0700

    selfheal_cosa_start_rem.sh: fix inconsistent indentations
    Signed-off-by: Rob Vogelaar <revogelaar@gmail.com>

--- scripts/selfheal_cosa_start_rem.sh
+++ scripts/selfheal_cosa_start_rem.sh
@@ -28,15 +28,15 @@ fi
 source /etc/utopia/service.d/log_capture_path.sh
 
 if [ -f /tmp/cosa_start_rem_triggered ]; then
-	echo "Already cosa_start_rem script triggered so no need to trigger again from selfheal"
-	rm -rf /etc/cron/cron.every10minute/selfheal_cosa_start_rem.sh
+    echo "Already cosa_start_rem script triggered so no need to trigger again from selfheal"
+    rm -rf /etc/cron/cron.every10minute/selfheal_cosa_start_rem.sh
 else
-	echo "cosa_start_rem script not triggered even after 10 minutes from boot-up so start from selfheal"
-	# some platforms like AXB3 need to run the following script,
-	# but some platforms like TCXB6 use systemd and does not need it,
-	# only start the script if it exists
-	if [ -f /usr/ccsp/cosa_start_rem.sh ]; then
-		sh /usr/ccsp/cosa_start_rem.sh &	
-	fi
-	rm -rf /etc/cron/cron.every10minute/selfheal_cosa_start_rem.sh
+    echo "cosa_start_rem script not triggered even after 10 minutes from boot-up so start from selfheal"
+    # some platforms like AXB3 need to run the following script,
+    # but some platforms like TCXB6 use systemd and does not need it,
+    # only start the script if it exists
+    if [ -f /usr/ccsp/cosa_start_rem.sh ]; then
+        sh /usr/ccsp/cosa_start_rem.sh &
+    fi
+    rm -rf /etc/cron/cron.every10minute/selfheal_cosa_start_rem.sh
 fi
