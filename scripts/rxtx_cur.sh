926d5b5d1de6f69ca839c268faa41f1afc7bf333
Rob Vogelaar <revogelaar@gmail.com>
2021-05-18 17:30:31 -0700

    rxtx_cur.sh: fix inconsistent indentations
    Signed-off-by: Rob Vogelaar <revogelaar@gmail.com>

--- scripts/rxtx_cur.sh
+++ scripts/rxtx_cur.sh
@@ -40,8 +40,8 @@ fi
 grep -E "192.168.245.|169.254.0.|169.254.1." /nvram/dnsmasq.leases | cut -d' ' -f2 | tr '[a-z]' '[A-Z]' > $PODMAC
 
 while read pmac; do
- pmac=$(echo $pmac | sed 's/\:/\\\:/g')
- sed -i "/$pmac/d" /tmp/rxtx_cur.txt
+    pmac=$(echo $pmac | sed 's/\:/\\\:/g')
+    sed -i "/$pmac/d" /tmp/rxtx_cur.txt
 done <$PODMAC
 
 ONETB=1099511627776
@@ -62,43 +62,45 @@ if [ ! -z "$high_upload_mac" ];then
     if [ $BOX_TYPE = "XB3" ]; then
         rpcclient2 "echo "High_UploadData_Usage_Client_Bytes:$high_upload_bytes" >> /rdklogs/logs/Harvesterlog.txt.0"
         rpcclient2 "echo "High_UploadData_Usage_Client_MAC:$high_upload_mac" >> /rdklogs/logs/Harvesterlog.txt.0"
-	t2ValNotify "HighUpldBytes_split" "$high_upload_bytes"
-	t2ValNotify "HighUpldMAC_split" "$high_upload_mac"
+        t2ValNotify "HighUpldBytes_split" "$high_upload_bytes"
+        t2ValNotify "HighUpldMAC_split" "$high_upload_mac"
     else
         echo "High_UploadData_Usage_Client_Bytes:$high_upload_bytes" >> /rdklogs/logs/Harvesterlog.txt.0
-	echo "High_UploadData_Usage_Client_MAC:$high_upload_mac" >> /rdklogs/logs/Harvesterlog.txt.0
-	t2ValNotify "HighUpldBytes_split" "$high_upload_bytes"
-	t2ValNotify "HighUpldMAC_split" "$high_upload_mac"
+        echo "High_UploadData_Usage_Client_MAC:$high_upload_mac" >> /rdklogs/logs/Harvesterlog.txt.0
+        t2ValNotify "HighUpldBytes_split" "$high_upload_bytes"
+        t2ValNotify "HighUpldMAC_split" "$high_upload_mac"
     fi
 fi
+
 if [ ! -z "$high_download_mac" ];then
     if [ $BOX_TYPE = "XB3" ]; then
         rpcclient2 "echo "High_DownloadData_Usage_Client_Bytes:$high_download_bytes" >> /rdklogs/logs/Harvesterlog.txt.0"
         rpcclient2 "echo "High_DownloadData_Usage_Client_MAC:$high_download_mac" >> /rdklogs/logs/Harvesterlog.txt.0"
-	t2ValNotify "HighDnldBytes_split" "$high_download_bytes"
-	t2ValNotify "HighDnldMAC_split" "$high_download_mac"
+        t2ValNotify "HighDnldBytes_split" "$high_download_bytes"
+        t2ValNotify "HighDnldMAC_split" "$high_download_mac"
     else
         echo "High_DownloadData_Usage_Client_Bytes:$high_download_bytes" >> /rdklogs/logs/Harvesterlog.txt.0
         echo "High_DownloadData_Usage_Client_MAC:$high_download_mac" >> /rdklogs/logs/Harvesterlog.txt.0
-	t2ValNotify "HighDnldBytes_split" "$high_download_bytes"
-	t2ValNotify "HighDnldMAC_split" "$high_download_mac"
+        t2ValNotify "HighDnldBytes_split" "$high_download_bytes"
+        t2ValNotify "HighDnldMAC_split" "$high_download_mac"
     fi
 fi
+
 cut -d'|' -f1 /tmp/rxtx_cur.txt | sort -u > /tmp/eblist
 # Dump leases table - strip out mesh pods
 grep -v "\* \*" /nvram/dnsmasq.leases | grep "192.168.245." | cut -d' ' -f2 > /tmp/cli47
 grep -v "\* \*" /nvram/dnsmasq.leases | grep -v "172.16.12." | grep -v "58:90:43" | grep -v "60:b4:f7" | grep -v "b8: ee :0e" | grep -v "b8:d9:4d" | cut -d' ' -f2 > /tmp/cli4
+
 if [ -z "$MAC" ]
 then
-	ip nei show | grep brlan0 | grep -v FAILED | cut -d' ' -f 5  > /tmp/cli46
+    ip nei show | grep brlan0 | grep -v FAILED | cut -d' ' -f 5  > /tmp/cli46
 else
-	ip nei show | grep brlan0 | grep -v $MAC | grep -v FAILED | cut -d' ' -f 5  > /tmp/cli46
+    ip nei show | grep brlan0 | grep -v $MAC | grep -v FAILED | cut -d' ' -f 5  > /tmp/cli46
 fi
 sort -u /tmp/cli4 /tmp/cli46 /tmp/cli47 | tr '[a-z]' '[A-Z]' > /tmp/clilist
 diff /tmp/eblist /tmp/clilist | grep "^+" | grep -v "+++" | cut -d'+' -f2 > /tmp/nclilist
 for mac in $(cat /tmp/nclilist); do
-  #ebtables -A INPUT -s $mac
-  #ebtables -A OUTPUT -d $mac
-  traffic_count -A $mac
+    #ebtables -A INPUT -s $mac
+    #ebtables -A OUTPUT -d $mac
+    traffic_count -A $mac
 done
-
