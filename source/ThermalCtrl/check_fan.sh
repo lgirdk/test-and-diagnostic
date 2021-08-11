e051e895162d2ba7471bddc6feff444891a62617
jmathe214 <jino_mathew@comcast.com>
2021-08-11 05:30:13 +0000

    RDKB-37199: Fan.2.RotorLock error
    Reason for change: CMXB7 has only one fan
    Test Procedure: TDMlog.txt.0 should not have dmcli throwing error
    Risks: Low
    Signed-off-by: Jino Mathew <Jino_Mathew@comcast.com>
    
    (cherry picked from commit d0b82bd317e0b46e23bcdecb4ec031d7f0b805b7)
    Change-Id: If325926a58e4faa9b9313d5549a3be1104c5dc64

--- source/ThermalCtrl/check_fan.sh
+++ source/ThermalCtrl/check_fan.sh
@@ -2,8 +2,17 @@
 
 source /etc/utopia/service.d/log_capture_path.sh
 
-rotorLock_1=`dmcli eRT getv Device.Thermal.Fan.1.RotorLock | grep value | awk '{print $5}'`
-rotorLock_2=`dmcli eRT getv Device.Thermal.Fan.2.RotorLock | grep value | awk '{print $5}'`
+rotorLock_1=""
+rotorLock_2=""
+
+NumberOfFan=`dmcli eRT getv Device.Thermal.FanNumberOfEntries | grep value | awk '{print $5}'`
+
+if [ "$NumberOfFan" = "1" ]; then
+	rotorLock_1=`dmcli eRT getv Device.Thermal.Fan.1.RotorLock | grep value | awk '{print $5}'`
+elif [ "$NumberOfFan" = "2" ]; then
+	rotorLock_1=`dmcli eRT getv Device.Thermal.Fan.1.RotorLock | grep value | awk '{print $5}'`
+	rotorLock_2=`dmcli eRT getv Device.Thermal.Fan.2.RotorLock | grep value | awk '{print $5}'`
+fi
 if [ "$rotorLock_1" = "True" ] || [ "$rotorLock_2" = "True" ]; then
     echo_t "THERMAL:Fan_Rotor_Lock"
 fi
