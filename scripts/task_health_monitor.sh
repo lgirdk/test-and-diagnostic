e7e30536cec9c05d484865ddc2d71df70e1dbbc7
Siddalingesh <sihiregoudar@libertyglobal.com>
2021-09-27 08:58:33 +0200

    don't restart TR069PA process in Bridge mode ( OFW-1854 )
    Test for bridge mode before restarting TR069PA.
    
    Signed-off-by: Sidda(sihiregoudar@libertyglobal.com)

--- scripts/task_health_monitor.sh
+++ scripts/task_health_monitor.sh
@@ -982,6 +982,7 @@ case $SELFHEAL_TYPE in
 
         #   fi
 
+        if [ "$BR_MODE" -eq 0 ]; then
         # Checking TR69's PID
         if [ "$MODEL_NUM" = "DPC3939B" ] || [ "$MODEL_NUM" = "DPC3941B" ] || [ "$MODEL_NUM" = "CGA4332COM" ]; then
             echo_t "BWG doesn't support TR069Pa "
@@ -995,6 +996,7 @@ case $SELFHEAL_TYPE in
                     resetNeeded TR69 CcspTr069PaSsp
                 fi
             fi
+        fi
         fi
 
         # Checking Test adn Daignostic's PID
