9743dc1a26bc369098a460ad988a79348c5dc3b8
Andre McCurdy <armccurdy@gmail.com>
2021-11-03 16:06:58 -0700

    task_health_monitor.sh: reset dmesg level after grep for "Atom only"
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- scripts/task_health_monitor.sh
+++ scripts/task_health_monitor.sh
@@ -657,7 +657,9 @@ case $SELFHEAL_TYPE in
         fi
         ########################################
         if [ "$BOX_TYPE" = "XB3" ]; then
-            atomOnlyReboot=$(dmesg -n 8 && dmesg | grep -i "Atom only")
+            dmesg -n 8
+            atomOnlyReboot=$(dmesg | grep -i "Atom only")
+            dmesg -n 5
             if [ "x$atomOnlyReboot" = "x" ]; then
                 crTestop=$(dmcli eRT getv com.cisco.spvtg.ccsp.CR.Name)
                 isCRAlive=$(echo "$crTestop" | grep "Can't find destination compo")
@@ -1272,8 +1274,9 @@ case $SELFHEAL_TYPE in
     "BASE")
     ;;
     "TCCBR")
-
-        atomOnlyReboot=$(dmesg -n 8 && dmesg | grep -i "Atom only")
+        dmesg -n 8
+        atomOnlyReboot=$(dmesg | grep -i "Atom only")
+        dmesg -n 5
         if [ "x$atomOnlyReboot" = "x" ]; then
             crTestop=$(dmcli eRT getv com.cisco.spvtg.ccsp.CR.Name)
             isCRAlive=$(echo "$crTestop" | grep "Can't find destination compo")
