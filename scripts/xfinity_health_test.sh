324bdc223baa6337bb3978bddcaf95e7254b2d6b
akarth252 <akilesh_karthikeyan@comcast.com>
2021-12-22 17:05:49 +0000

    RDKB-36678 : Maintenance window script
    Reason for change: to perform xfinity health test during maintenance
    Test Procedure: function must happen during maintenance window
    Risks: Low
    Signed-off-by:akilesh_karthikeyan@comcast.com
    
    (cherry picked from commit fdfb3b8d987f0f3011517738f1d5f10012329e90)
    Change-Id: If209985ba2ba456e9b1ab0f6afe140dd4f2ed0a4

---
+++ scripts/xfinity_health_test.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+source /usr/ccsp/tad/corrective_action.sh
+xfinitytestlogfile=/rdklogs/logs/xfinityTestAgent.log
+xfinityenable=`psmcli get dmsb.hotspot.enable`
+testenable=`syscfg get XfinityHealthCheckEnable`
+testcadence=`syscfg get XfinityHealthCheckCadence`
+daystotest=`syscfg get XfinityHealthCheckRemDays`
+donetoday=`syscfg get XfinityHealthCheckDone`
+
+if [ "$xfinityenable" = "1" ] && [ "$testenable" = "true" ];then
+    checkMaintenanceWindow
+    if [ $reb_window -eq 1 ];then
+        if [ "$donetoday" = "false" ];then
+            if [ "$daystotest" = "0" ];then
+                /usr/bin/xfinitytest brTest 4091
+                syscfg set XfinityHealthCheckRemDays $((--testcadence));
+            else
+                syscfg set XfinityHealthCheckRemDays $((--daystotest));
+                echo `date` ": HOTSPOT_HEALTHCHECK :" $daystotest "days till Healthcheck" >> $xfinitytestlogfile
+            fi
+            syscfg set XfinityHealthCheckDone true;syscfg commit
+        fi
+    else
+        echo `date` ": HOTSPOT_HEALTHCHECK : Not in Maintenance window" >> $xfinitytestlogfile
+        syscfg set XfinityHealthCheckDone false;syscfg commit
+    fi
+else
+    echo `date` ": HOTSPOT_HEALTHCHECK : Feature is disabled" >> $xfinitytestlogfile
+fi
+
