9a6135ad4f4b57954073da214508230b2cd959c5
mkamat <mkamate@libertyglobal.com>
2022-01-19 10:11:39 -0800

    fix GPV returns default value even after successful SPV on Device.IP.Diagnostics.TraceRoute.Timeout ( OFW-2292 )
    The data model object timeout values are in milliseconds and need to
    be stored as millisecond values so that they can be read back.
    
    Since Busybox versions of traceroute etc don't currently accept
    floating point timeout values, convert from milliseconds to seconds
    when the timeouts are used.

--- source/dmltad/cosa_ip_dml.c
+++ source/dmltad/cosa_ip_dml.c
@@ -1642,7 +1642,7 @@ IPPing_GetParamUlongValue
     if (strcmp(ParamName, "NumberOfRepetitions") == 0)
         *puLong = cfg.cnt;
     else if (strcmp(ParamName, "Timeout") == 0)
-        *puLong = cfg.timo * 1000;
+        *puLong = cfg.timo;
     else if (strcmp(ParamName, "DataBlockSize") == 0)
         *puLong = cfg.size;
     else if (strcmp(ParamName, "DSCP") == 0)
@@ -1905,7 +1905,7 @@ IPPing_SetParamUlongValue
             cfg.cnt = uValue;
     }
     else if (strcmp(ParamName, "Timeout") == 0)
-        cfg.timo = uValue / 1000;
+        cfg.timo = uValue;
     else if (strcmp(ParamName, "DataBlockSize") == 0)
     {
         cfg.size = uValue;
@@ -2346,7 +2346,7 @@ TraceRoute_GetParamUlongValue
     if (strcmp(ParamName, "NumberOfTries") == 0)
         *puLong = cfg.cnt;
     else if (strcmp(ParamName, "Timeout") == 0)
-        *puLong = cfg.timo * 1000;
+        *puLong = cfg.timo;
     else if (strcmp(ParamName, "DataBlockSize") == 0)
         *puLong = cfg.size;
     else if (strcmp(ParamName, "DSCP") == 0)
@@ -2598,7 +2598,7 @@ TraceRoute_SetParamUlongValue
     if (strcmp(ParamName, "NumberOfTries") == 0)
         cfg.cnt = uValue;
     else if (strcmp(ParamName, "Timeout") == 0)
-        cfg.timo = uValue / 1000;
+        cfg.timo = uValue;
     else if (strcmp(ParamName, "DataBlockSize") == 0)
         cfg.size = uValue;
     else if (strcmp(ParamName, "DSCP") == 0)
