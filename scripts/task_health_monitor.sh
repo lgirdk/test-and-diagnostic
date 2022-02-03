2c19d90596d825dde7e91c609d33bf3bfe6394a0
Dhanya <dravindran.contractor@libertyglobal.com>
2022-02-03 17:27:56 +0100

    task_health_monitor.sh: fix Selfheal restart of telemetry ( Part of OFW-2227, CH7465SI-129 )
    [Description]
    When telemetry process crashes, Selfheal is not recovering it.
    
    [Solution]
    Added code to start the telemetry process if telemetry enable is true but the process is not running.
    
    [Testing Performed]
    Verified the telemetry is restarted by killing the telemetry process.
    
    Signed-off-by: Dhanya Ravindran<dravindran.contractor@libertyglobal.com>

--- scripts/task_health_monitor.sh
+++ scripts/task_health_monitor.sh
@@ -4775,9 +4775,7 @@ if [ "$T2_ENABLE" = "true" ]; then
     T2_PID=$(get_pid $T2_0_APP)
     if [ "$T2_PID" = "" ]; then
         echo_t "RDKB_PROCESS_CRASHED : $T2_0_APP is not running, need restart"
-        if [ -f /lib/rdk/dcm.service ]; then 
-            /lib/rdk/dcm.service
-        fi
+        /usr/bin/telemetry2_0 &
     fi
 fi
 
