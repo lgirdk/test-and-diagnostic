279d020800faf51ebcdc00145e9a7b7973acbe87
abhishekmohapatra92 <amohapatra.contractor@libertyglobal.com>
2022-07-07 18:01:54 +0530

    Revert "fix Telemetry RDKB used memory events not working properly ( OFW-2245 CH7465SI-136 CH7465OFW-810 )"
    This reverts commit 5751dfc69f76b73b13b6ede0a2ca2a3f37eb7003.

--- scripts/log_mem_cpu_info.sh
+++ scripts/log_mem_cpu_info.sh
@@ -72,7 +72,7 @@ logTmpFs()
       echo_t "================================================================================"
    fi
 
-   t2ValNotify "TMPFS_USE_PERCENTAGE_split" "$TMPFS_CUR_USAGE"
+   t2ValNotify "TMPFS_USE_PERCENTAGE" "$TMPFS_CUR_USAGE"
 }
 
 get_high_mem_processes() {
@@ -280,18 +280,18 @@ get_high_mem_processes() {
 	count=$((count + 1))
 
 	RDKLOGS_USAGE=`df /rdklogs | tail -1 | awk '{print $(NF-1)}' | cut -d"%" -f1`
-	t2ValNotify "RDKLOGS_USE_PERCENTAGE_split" "$RDKLOGS_USAGE"
+	t2ValNotify "RDKLOGS_USE_PERCENTAGE" "$RDKLOGS_USAGE"
 
 	NVRAM_USAGE=`df /nvram | tail -1 | awk '{print $(NF-1)}' | cut -d"%" -f1`
-	t2ValNotify "NVRAM_USE_PERCENTAGE_split" "$NVRAM_USAGE"
+	t2ValNotify "NVRAM_USE_PERCENTAGE" "$NVRAM_USAGE"
 
 	swap=`free | awk 'FNR == 4 {print $3}'`
 	cache=`cat /proc/meminfo | awk 'FNR == 4 {print $2}'`
 	buff=`cat /proc/meminfo | awk 'FNR == 3 {print $2}'`
 
-	t2ValNotify "SWAP_MEMORY_split" "$swap"
-	t2ValNotify "CACHE_MEMORY_split" "$cache"
-	t2ValNotify "BUFFER_MEMORY_split" "$buff"
+	t2ValNotify "SWAP_MEMORY" "$swap"
+	t2ValNotify "CACHE_MEMORY" "$cache"
+	t2ValNotify "BUFFER_MEMORY" "$buff"
 
 	nvram_ro_fs=`mount | grep "nvram " | grep dev | grep "[ (]ro[ ,]"`
 	if [ "$nvram_ro_fs" != "" ]; then
