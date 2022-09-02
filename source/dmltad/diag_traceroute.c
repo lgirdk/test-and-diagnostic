3e9e5994d26ccc98a32abaeed48144d6ecdd68df
Andre McCurdy <armccurdy@gmail.com>
2022-09-02 16:21:16 -0700

    support setting ping or traceroute hostname to an empty string ( OFW-113 OFW-288 )
    Host has data type string(0:256) and setting to an empty string
    should be allowed. Setting an empty string is useful for clearing a
    previously set hostname.
    
    Verified that below dmcli commands succeed:
    
      dmcli eRT setv Device.IP.Diagnostics.IPPing.Host string ""
      dmcli eRT setv Device.IP.Diagnostics.TraceRoute.Host string ""
    
    Return Error_CannotResolveHostName error if host is an empty string.
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source/dmltad/diag_traceroute.c
+++ source/dmltad/diag_traceroute.c
@@ -192,8 +192,11 @@ static diag_err_t tracert_start(diag_obj_t *diag, const diag_cfg_t *cfg, diag_st
     diag_err_t err = DIAG_ERR_INTERNAL;
     assert(diag == &diag_tracert);
 
-    if (!cfg || !strlen(cfg->host) || !stat)
+    if (!cfg || !stat)
         return DIAG_ERR_PARAM;
+
+    if (strlen(cfg->host) == 0)
+        return DIAG_ERR_RESOLVE;
 
 #if !defined(_PLATFORM_RASPBERRYPI_)
     timeout = cfg->timo;
