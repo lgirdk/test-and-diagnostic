52b5ad01db5eba2f730be1d214961673a3112dde
Abhijeet Viswam <Abhijeet_Viswam@comcast.com>
2023-04-06 14:33:08 +0000

    RDKB-48205 : Cron schedule to run xmesh_diagnostics every six hours
    Reason for change: Cron schedule to run xmesh_diagnostics
                       every six hours
    Test Procedure: See user story acceptance criteria
    
    Priority: P1
    Risks: Low
    Signed-off-by: Abhijeet Viswam <Abhijeet_Viswam@comcast.com>
    (cherry picked from commit 93defe3f08f62cfdc4e1f2bf8eeffb1e313c3b43)
    Change-Id: Ia868e9ba84f1135bdc7a3efdd91be1819095525c

--- scripts/log_sixhourly.sh
+++ scripts/log_sixhourly.sh
@@ -19,6 +19,8 @@
 ##########################################################################
 
 source /etc/utopia/service.d/log_capture_path.sh
+source /etc/device.properties
+
 MAPT_CONFIG=`sysevent get mapt_config_flag`
 dt=`date "+%m-%d-%y-%I-%M%p"`
 ps -l | grep '^Z' > /tmp/zombies.txt
@@ -26,25 +28,49 @@ count=`wc -l < /tmp/zombies.txt`
 echo "************ ZOMBIE_COUNT $count at $dt ************"
 
 if [ $count -ne 0 ];then
-	echo "*************** List of Zombies ***************"
-	cat /tmp/zombies.txt
-	echo "***********************************************"
+    echo "*************** List of Zombies ***************"
+    cat /tmp/zombies.txt
+    echo "***********************************************"
 fi
 rm /tmp/zombies.txt
 
 if [ "xstarted" == "x`sysevent get wan-status`" ];then
-        if [ "$MAPT_CONFIG" != "set" ]; then
-	if [  "x`ip -4 route show table erouter | grep "default via" | grep erouter0 | cut -f3 -d' ' `" == "x" ]; then
-		echo "ipv4 default gateway is missing" >> /rdklogs/logs/Consolelog.txt.0
+    if [ "$MAPT_CONFIG" != "set" ]; then
+        if [  "x`ip -4 route show table erouter | grep "default via" | grep erouter0 | cut -f3 -d' ' `" == "x" ]; then
+            echo "ipv4 default gateway is missing" >> /rdklogs/logs/Consolelog.txt.0
         fi
-        elif [  "x`ip -4 route show table erouter | grep "default via" | grep map0 | cut -f3 -d' ' `" == "x" ]; then
-                echo "ipv4 map0 default gateway is missing" >> /rdklogs/logs/Consolelog.txt.0
-	fi
-       
- 
-	if [ "x`ip -6 route show | grep "default via" | grep erouter0 | grep -i "dead:beef"`"  != "x" ];then
-        	echo "Device is connected to virtual cmts, default ipv6 erouter entry not needed in erouter table"  >> /rdklogs/logs/Consolelog.txt.0
-	elif [  "x`ip -6 route show table erouter | grep "default via" | grep erouter0 | cut -f3 -d' ' `" == "x" ]; then
-		echo "ipv6 default gateway is missing" >> /rdklogs/logs/Consolelog.txt.0
-	fi
+    elif [  "x`ip -4 route show table erouter | grep "default via" | grep map0 | cut -f3 -d' ' `" == "x" ]; then
+        echo "ipv4 map0 default gateway is missing" >> /rdklogs/logs/Consolelog.txt.0
+    fi
+
+
+    if [ "x`ip -6 route show | grep "default via" | grep erouter0 | grep -i "dead:beef"`"  != "x" ];then
+        echo "Device is connected to virtual cmts, default ipv6 erouter entry not needed in erouter table"  >> /rdklogs/logs/Consolelog.txt.0
+    elif [  "x`ip -6 route show table erouter | grep "default via" | grep erouter0 | cut -f3 -d' ' `" == "x" ]; then
+        echo "ipv6 default gateway is missing" >> /rdklogs/logs/Consolelog.txt.0
+    fi
+fi
+
+# Mesh diagnostics logs for XLE and XB7
+WFO_ARG=
+# Skip first iteration since device would be still initializing.
+UPTIME=`cat /proc/uptime | cut -d' ' -f1 | cut -d'.' -f1`
+if [ "$UPTIME" -gt 600 ]; then
+    if [ "$MODEL_NUM" == "CGM4331COM" ] || [ "$MODEL_NUM" == "TG4482A" ]; then
+        ACT_IFACE=`dmcli eRT getv Device.X_RDK_WanManager.CurrentActiveInterface | grep 'value: ' | cut -d':' -f3 | xargs`
+        if [ "$ACT_IFACE" == "brRWAN" ]; then
+            WFO_ARG="-w"
+        fi
+        echo  "================== Periodic xmesh_diagnostics logging ==================" >> /rdklogs/logs/MeshBlackbox.log
+        echo  "================== Periodic xmesh_diagnostics logging ==================" >> /rdklogs/logs/MeshBlackboxDumps.log
+        xmesh_diagnostics -d $WFO_ARG
+    elif [ "$MODEL_NUM" == "WNXL11BWL" ]; then
+        WFO_ENABLED=`sysevent get mesh_wfo_enabled`
+        if [ "$WFO_ENABLED" == "true" ]; then
+            WFO_ARG="-w"
+        fi
+        echo  "================== Periodic xmesh_diagnostics logging ==================" >> /rdklogs/logs/MeshBlackbox.log
+        echo  "================== Periodic xmesh_diagnostics logging ==================" >> /rdklogs/logs/MeshBlackboxDumps.log
+        xmesh_diagnostics -d $WFO_ARG
+    fi
 fi
