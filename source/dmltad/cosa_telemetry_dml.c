f1b84d69794a72db55edf1a72ab06b24d911a2d0
Devin Mungan <dmungan@libertyglobal.com>
2023-04-17 14:50:40 +0200

    run telemetry2_0 and icu utility with low priority ( OFW-4219 CH7465SI-620 )
    fix RDK-B logging in Mv1 causing high DNS delay
    
    [Issue]
    During periodic telemetry uploads,
    CPU spikes and DNS queries get delayed.
    
    [Solution]
    - Increase the CPU priority of dnsmasq process
    - Decrease the CPU priority of telemetry process
    
    [Testing Performed]
    - check the priorities of telemetry daemon and dnsmasq with below command.
    ps axl | grep '[d]nsmasq\|[t]elemetry2_0' | grep -v telemetry2_0_client | awk '{print $5" "$13}'
    - The output should look like below (dnsmasq prio:10, telemetry2_0 prio:30)
    10 dnsmasq
    10 dnsmasq
    30 telemetry2_0
    
    Upstream Candidate: No.
    
    Signed-off-by: Devin Mungan(dmungan@libertyglobal.com)

--- source/dmltad/cosa_telemetry_dml.c
+++ source/dmltad/cosa_telemetry_dml.c
@@ -205,9 +205,12 @@ BOOL Telemetry_Commit ( ANSC_HANDLE hInsContext )
                 CcspTraceInfo(("%s: Telemetry is enabled, downloading DCMConfigFile\n", __FUNCTION__));
                 CcspTraceInfo(("%s: Starting  Telemetry service\n", __FUNCTION__));
 #ifdef _PUMA6_ARM_
-                system("/usr/sbin/icu -R -B -p 192.168.254.253:2222; rpcclient2 '/usr/sbin/icu -R -B -p 192.168.254.254:2222'");
-#endif
+                system("nice -n 10 /usr/sbin/icu -R -B -p 192.168.254.253:2222 ; "
+                       "rpcclient2 '/usr/sbin/icu -R -B -p 192.168.254.254:2222' ; "
+                       "nice -n 10 /usr/bin/telemetry2_0 &");
+#else
                 system("/usr/bin/telemetry2_0 &");
+#endif
                 break;
             }
 	    case TELE_ST_STOP:
