72d1d3408c2e2ce1cd24af019bf2e1061ce97967
Andre McCurdy <armccurdy@gmail.com>
2023-05-16 15:38:07 -0700

    drop obsolete references to UDHCPEnable / UDHCPEnable_v2 syscfg variable
    Always assumed to be true in OFW builds (although the code which
    tests it is conditional on XB3 or Puma 7, so is never compiled for
    OFW based builds anyway... and the syscfg default value has always
    been wrong).
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- scripts/task_health_monitor.sh
+++ scripts/task_health_monitor.sh
@@ -4005,7 +4005,7 @@ if [ "$BOX_TYPE" != "HUB4" ] && [ "$BOX_TYPE" != "SR300" ] && [ "$BOX_TYPE" != "
 
     case $SELFHEAL_TYPE in
         "BASE"|"SYSTEMD")
-            UDHCPC_Enable=$(syscfg get UDHCPEnable_v2)
+            UDHCPC_Enable="true"
             dibbler_client_enable="true"
 
 	    if ( ( [ "$MANUFACTURE" = "Technicolor" ] || [ "$MANUFACTURE" = "Sercomm" ] ) && [ "$BOX_TYPE" != "XB3" ] ) || [ "$WAN_TYPE" = "EPON" ] || [ "$BOX_TYPE" = "VNTXER5" ] || [ "$BOX_TYPE" = "SCER11BEL" ]; then
