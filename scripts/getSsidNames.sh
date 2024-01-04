8c2735a94c81e265447b4e1d062675cecb2fec1f
Sanjay Venkatesan <Sanjay_Venkatesan@Comcast.com>
2024-01-04 10:40:06 +0000

    TCXB8-2811 RDKB-53404:
    Impacted Platforms: All rdkb platforms
    
    Reason for change: Telemetry markers are'nt configured for Radio3(6GHz).
    
    Test Procedure:
    1. Load the device with required build.
    2. Configure the 2G and 5G with same SSID, but not for 6G SSID.
    3. Monitor For "SYS_INFO_sameSSID" marker for every 6 hrs.
    
    Risks: Low
    
    Priority: P1
    
    Change-Id: I1402c0719d215c1672cebe4899b59ccb4998af6f
    Signed-off-by: Sanjay_Venkatesan@comcast.com
    (cherry picked from commit 3eeaf8608e2fdb3df304b8e1555f10c3c9ef1f9c)

--- scripts/getSsidNames.sh
+++ scripts/getSsidNames.sh
@@ -27,6 +27,12 @@ ssid24value=""
 ssid5value=""
 got_24=0
 got_5=0
+if [ "$triband_radio" == "true" ]; then
+    radioenable_6=$(dmcli eRT getv Device.WiFi.Radio.3.Enable)
+    isRadioEnabled_6=$(echo "$radioenable_6" | grep "false")
+    ssid6value=""
+    got_6=0
+fi
 if [ "x$BOX_TYPE" = "xXB3" ]; then
 GET_PID_FROM_PEER=`rpcclient $ATOM_ARPING_IP "busybox pidof CcspWifiSsp"`
 WiFi_PID=`echo "$GET_PID_FROM_PEER" | awk 'END{print}' | grep -v "RPC CONNECTED"`
@@ -58,21 +64,64 @@ if [ "x$WiFi_PID" != "x" ]; then
     else
         got_5=1
     fi
-    
-    # Compare 2.4GHz and 5GHz SSID and log
-    if [ $got_24 -eq 1 ] && [ $got_5 -eq 1 ]
-    then
-         if [ "$ssid5value" == "$ssid24value" ]
-         then
-            echo "`date +'%Y-%m-%d:%H:%M:%S:%6N'` [RDKB_STAT_LOG] 2.4G and 5G SSIDs are same"
-    	t2CountNotify "SYS_INFO_sameSSID"
-         else
-            echo "`date +'%Y-%m-%d:%H:%M:%S:%6N'` [RDKB_STAT_LOG] 2.4G and 5G SSIDs are different"
-    	t2CountNotify "SYS_INFO_differentSSID"
-         fi
-         got_24=0
-         got_5=0 
+
+    if [ "$triband_radio" == "true" ]; then
+        ssid6value=`dmcli eRT retv Device.WiFi.SSID.17.SSID`
+        # Get 6GHz SSID and do sanity check
+        if [ "$isRadioEnabled_6" == "" ]; then
+            if [ "$ssid6value" = "" ]; then
+                echo "`date +'%Y-%m-%d:%H:%M:%S:%6N'` [RDKB_PLATFORM_ERROR] Didn't get WiFi 6 GHz SSID from agent"
+                t2CountNotify "WIFI_ERROR_atomConsoleDown_6G"
+            else
+                got_6=1
+            fi
+        else
+            echo "Radio 3(6GHz) is not enabled"
+        fi
+
+        # Compare 2.4GHz, 5GHz and 6GHz SSID and log
+
+        isRadioExecutionSucceed_6=$(echo "$radioenable_6" | grep "Execution succeed")
+
+        if [ "$isRadioExecutionSucceed_6" != "" ]; then
+            isRadioEnabled_6=$(echo "$radioenable_6" | grep "false")
+
+            if [ "$isRadioEnabled_6" == "" ]; then
+                if [ $got_24 -eq 1 ] && [ $got_5 -eq 1 ] && [ $got_6 -eq 1 ]; then
+                    if [[ "$ssid6value" == "$ssid5value" && "$ssid5value" == "$ssid24value" ]]; then
+                        echo "`date +'%Y-%m-%d:%H:%M:%S:%6N'` [RDKB_STAT_LOG] 2.4G, 5G and 6G SSIDs are same"
+                        t2CountNotify "SYS_INFO_sameSSID"
+                    else
+                        echo "`date +'%Y-%m-%d:%H:%M:%S:%6N'` [RDKB_STAT_LOG] 2.4G, 5G and 6G SSIDs are different"
+                        t2CountNotify "SYS_INFO_differentSSID"
+                    fi
+                else
+                    echo "Failed to fetch SSID's from their respective radios"
+                    got_6=0
+                fi
+            else
+                echo "Radio 3(6GHz) was not enabled"
+            fi
+        else
+            echo "Radio execution got failed"
+        fi
+
+    else
+        # Triband radio is'nt present
+        # Compare 2.4GHz and 5GHz SSID and log
+        if [ $got_24 -eq 1 ] && [ $got_5 -eq 1 ]; then
+            if [ "$ssid5value" == "$ssid24value" ]; then
+                echo "`date +'%Y-%m-%d:%H:%M:%S:%6N'` [RDKB_STAT_LOG] 2.4G and 5G SSIDs are same"
+                t2CountNotify "SYS_INFO_sameSSID"
+            else
+                echo "`date +'%Y-%m-%d:%H:%M:%S:%6N'` [RDKB_STAT_LOG] 2.4G and 5G SSIDs are different"
+                t2CountNotify "SYS_INFO_differentSSID"
+            fi
+        fi
+        got_24=0
+        got_5=0
     fi
+
 else
     echo "`date +'%Y-%m-%d:%H:%M:%S:%6N'` [RDKB_PLATFORM_ERROR] WiFi Agent is not running skipping getSsidNames"
     t2CountNotify "WIFI_INFO_skipSSID"
