480af4b4db6d8fa1c5b21d20117813fdb9eac1e7
qlei <rlei@libertyglobal.com>
2024-04-25 16:56:25 -0700

    remove dependency on dbus

--- source/TandDSsp/ssp_messagebus_interface.c
+++ source/TandDSsp/ssp_messagebus_interface.c
@@ -47,7 +47,6 @@
         Service.
 
         *   ssp_TadMbi_MessageBusEngage
-        *   ssp_TadMbi_EventCallback
 
     ---------------------------------------------------------------
 
@@ -144,25 +143,6 @@ ssp_TadMbi_MessageBusEngage
 
     CcspBaseIf_SetCallback(bus_handle, &cb);
 
-
-    /* Register service callback functions */
-    returnStatus =
-        CCSP_Message_Bus_Register_Path
-            (
-                bus_handle,
-                path,
-                CcspTandD_path_message_func,
-                bus_handle
-            );
-
-    if ( returnStatus != CCSP_Message_Bus_OK )
-    {
-        CcspTraceError((" !!! CCSP_Message_Bus_Register_Path ERROR returnStatus: %lu\n!!!\n", returnStatus));
-
-        return returnStatus;
-    }
-
-
     /* Register event/signal */
     returnStatus =
         CcspBaseIf_Register_Event
@@ -181,36 +161,6 @@ ssp_TadMbi_MessageBusEngage
 
     return ANSC_STATUS_SUCCESS;
 }
-
-DBusHandlerResult
-CcspTandD_path_message_func
-    (
-        DBusConnection  *conn,
-        DBusMessage     *message,
-        void            *user_data
-    )
-{
-    CCSP_MESSAGE_BUS_INFO *bus_info =(CCSP_MESSAGE_BUS_INFO *) user_data;
-    const char *interface = dbus_message_get_interface(message);
-    const char *method   = dbus_message_get_member(message);
-    DBusMessage *reply;
-    reply = dbus_message_new_method_return (message);
-    if (reply == NULL)
-    {
-        return DBUS_HANDLER_RESULT_HANDLED;
-    }
-
-    return CcspBaseIf_base_path_message_func
-               (
-                   conn,
-                   message,
-                   reply,
-                   interface,
-                   method,
-                   bus_info
-               );
-}
-
 
 int
 ssp_TadMbi_Initialize
